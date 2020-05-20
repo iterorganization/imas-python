1d3e438840cb8fc5d9dbbbdd63c4c9e5427dc075
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2020-05-20 23:44:57 +0200

    Make a very basic MVP script

--- pymas/examples/ids_dummy_class.py
+++ pymas/examples/ids_dummy_class.py
@@ -2,6 +2,8 @@ import sys
 import os
 
 from IPython import embed
+import numpy
+import numpy as np
 
 
 if __name__ == '__main__':
@@ -234,14 +236,156 @@ if __name__ == '__main__':
             if status != 0:
              raise ALException('Error calling ual_end_action() for amns_data', status)
 
-    dummy = DummyIDS()
+    #dummy = DummyIDS('dummy')
     # MVP, get a slice
     # So for example be able to run
     # import pymas as imas
     # input = imas.ids(shot,run_in)
     # input.open_env(input_user_or_path,input_database,’3’)
     # input.equilibrium.getSlice(time_slice, 1)
-    import pymas as imas
-    inp = imas.ids
-    inp.open_env(input_user_or_path, input_database, '3')
-    embed()
+
+    # Existing workflow example
+    shot               = 130012
+    run_in             = 1
+    #input_user_or_path = 'public'
+    input_database     = 'iter'
+    time_slice         = 200
+    occurence = 0
+
+    # Test example
+    TEST_SHOT = 1
+    TEST_RUN = 2
+    backend = 3
+    BACKEND_ID_0 = 10
+    MEMORY_BACKEND              = BACKEND_ID_0+4;
+    backend = MEMORY_BACKEND
+    ACCESS_PULSE_0  = 40
+    FORCE_CREATE_PULSE     =     ACCESS_PULSE_0+3
+    input_user_or_path = 'vandepk'
+    # db_obj =  db_entry(backend, 'test', TEST_SHOT, TEST_RUN)
+    # Create a new entry
+    status, idx = ull.ual_begin_pulse_action(backend, shot, run_in, input_user_or_path, input_database, '3')
+    if status != 0:
+        raise Exception('Error calling ual_begin_pulse_action()')
+    status = ull.ual_open_pulse(idx, FORCE_CREATE_PULSE, '') 
+    db_ctx = idx 
+
+
+    # Old API
+    #import matplotlib.pyplot as plt
+    style = 'old'
+    style = 'new'
+    if style == 'old':
+        import imas_3_28_1_ual_4_7_2_dev38 as imas
+        #inp_old = imas.ids(shot,run_in)
+        #inp_old.open_env(input_user_or_path,input_database, '3')
+        #time = inp_old.equilibrium.readTime(occurence)
+        #inp_old.equilibrium.getSlice(time_slice, 1)
+        #inp_old.close()
+        #timeslice = inp_old.equilibrium.time_slice[0]
+        #print('Using code {!s}'.format(inp_old.equilibrium.code.name))
+        #print('time =', time)
+        #print(timeslice.profiles_1d.psi)
+        #print(timeslice.profiles_1d.b_average)
+        # Other magic
+
+        imas_entry = imas.ids(shot, run_in)
+        idx = imas_entry.create_env(input_user_or_path, input_database, '3')
+        ids = imas_entry.equilibrium
+        #ids.ids_properties.homogeneous_time = 2
+        time = 2
+        ids.ids_properties.homogeneous_time = time
+        ids.setPulseCtx(db_ctx)
+        ids.put()
+        print('Temp database made')
+        print('Original ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
+        ids.ids_properties.homogeneous_time = 'foo'
+        print('Changed ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
+        ids.get()
+        print('After re-get ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
+    else:
+        # New API
+        # 4.4.1 Initializing an empty IDS
+        import pymas as imas
+        idsdef = '../../../IDSDef.xml'
+        imas_entry = imas.ids(shot, run_in, xml_path=idsdef)
+        # This should then support accessing an IDS using idsVar.<IDSname>.<IDSfield>
+
+        # 5.5 Opening an existing Data Entry
+        #imas_entry.open_env(input_user_or_path, input_database, '3')
+
+        # 5.6 Opening a remote Data Entry using the UDA backend
+        #imas_entry = imas.ids(54178, 0)
+        #imas_entry.open_public('WEST')
+
+        # 5.7 Closing an opened Data Entry
+        # [retstatus] = imas_entry.close()
+
+        # 4.4.2 Copying a variable to a new IDS. Not deepcopy! Proposed API:
+        # idsVar2.equilibrium.copyValues(idsVar)
+
+        # 4.4.3 Deallocation
+        # Using python build-in management, or del var
+
+        # 5.4 Creating a new data entry
+        imas_entry.create_env(input_user_or_path, input_database, '3')
+        # Put mandatory top-level entry
+        ids = imas_entry.equilibrium
+        #ids.ids_properties.homogeneous_time = 2
+        time = 2
+        ids.ids_properties.homogeneous_time = time
+        ids.setPulseCtx(db_ctx)
+        ids.put()
+        #ids.ids_properties.homogeneous_time = 'crap'
+        print('Done putting')
+        print('Putting')
+
+        #ids.get(ctx=db_ctx, homogeneousTime=time)
+        print('Temp database made')
+        print('Original ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
+        ids.ids_properties.homogeneous_time = 9999
+        print('Changed ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
+        ids.get()
+        print('After re-get ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
+        embed()
+
+        # 5.8 Putting an IDS
+        # imas_entry.pf_active.put(occurence)
+
+        # 5.9 Putting an IDS progressively in a time loop
+        # Expect the IDS variable to contain a single slice in any non-empty dynamic nodes
+        # So, start with a single .put, then progressive put_slice IN STRICT TIME ORDER
+
+        # 5.10 Deleting an IDS
+        # imas_entry.pf_active.deleteData(occurence)
+
+        # 5.11 getting an IDS
+        # imas_entry.pf_active.get(occurence)
+
+        # 5.12 Getting a slice
+        # imas_entry.pf_active.getSlice(0.1, 2)
+
+        # 5.13 Getting units of a note
+        # Slash-separated path, ignore arrays, as all units in array are the same
+        # units = get_units(ids, node_path)
+
+        # 5.14 Getting a subset of an IDS
+        # Smart datapath, so path/to/array(idx)/field
+        # Smart datapath, so path/to/array(x:y)/field
+        # partialGet
+
+        # 5.15 Selecting the backend
+        # MDSPLUS, MEMORY, UDA, ASCII
+        # MAGIC, ignore for now?
+
+        # Grab a direct child node of primitive type FLT_1D. It's dynamic, so special, and a coordinate, double special
+        #time = imas_entry.equilibrium.readTime(occurence)
+        # Grab a child structure
+        #print(time)
+        #e.g. ids_entry = imas.ids(12,1)
+        #inp.equilibrium.getSlice(time_slice, 1)
+        #timeslice = inp.equilibrium.time_slice[0]
+        # retstatus, idx = ids_entry.create_env('usr', 'test', '3')
+    #plt.plot(timeslice.profiles_1d.psi, timeslice.profiles_1d.b_average)
+    #plt.show()
+
--- pymas/ids_classes.py
+++ pymas/ids_classes.py
@@ -1,6 +1,49 @@
 import abc
+import imas
+from IPython import embed
+import imas_3_28_1_ual_4_7_2_dev38._ual_lowlevel as ull
+from .imasdef import MDSPLUS_BACKEND, OPEN_PULSE, DOUBLE_DATA, READ_OP, EMPTY_INT, FORCE_CREATE_PULSE, IDS_TIME_MODE_UNKNOWN,IDS_TIME_MODES, IDS_TIME_MODE_HOMOGENEOUS, WRITE_OP, CHAR_DATA, INTEGER_DATA
+import numpy as np
+import xml
+import xml.etree.ElementTree as ET
+import pymas.hli_utils as hli_utils
+
+class ALException(Exception):
+
+   def __init__(self, message, errorStatus=None):
+        if errorStatus is not None:
+          Exception.__init__(self, message + "\nError status=" + str(errorStatus))
+        else:
+          Exception.__init__(self, message)
+
+class IDSPrimitive():
+    pass
+
+# For now, hard code this, sorry!
+class IDS_STR_0D(IDSPrimitive):
+    pass
+
+class IDS_INT_0D(IDSPrimitive):
+    pass
+
+data_type_to_class = {
+    'STR_0D': IDS_STR_0D,
+    'INT_0D': IDS_INT_0D,
+}
+
+data_type_to_default = {
+    'STR_0D': '',
+    'INT_0D': EMPTY_INT,
+}
+python_type_to_ual = {
+    str: 'STR_0D',
+    int: 'INT_0D',
+}
+
 class IDSRoot():
- def __init__(self, s=-1, r=-1, rs=-1, rr=-1):
+
+ def __init__(self, s=-1, r=-1, rs=-1, rr=-1, xml_path=None):
+  setattr(self, 'shot', s)
   self.shot = s
   self.refShot = rs
   self.run = r
@@ -8,10 +51,25 @@ class IDSRoot():
   self.treeName = 'ids'
   self.connected = False
   self.expIdx = -1
-  self.ddunits = DataDictionaryUnits()
-  self.hli_utils = HLIUtils()
-  self.amns_data = amns_data.amns_data()
-  self.barometry = barometry.barometry()
+  XMLtreeIDSDef = ET.parse(xml_path)
+  root = XMLtreeIDSDef.getroot()
+  self._children = []
+  for ids in root:
+      my_name = ids.get('name')
+      if my_name is None:
+          continue
+      # Only build for equilibrium to KISS
+      if my_name != 'equilibrium':
+          continue
+      self._children.append(my_name)
+      setattr(self, my_name, IDSToplevel(my_name, ids))
+  #self.equilibrium = IDSToplevel('equilibrium')
+
+  # Do not use this now
+  #self.ddunits = DataDictionaryUnits()
+  #self.hli_utils = HLIUtils()
+  #self.amns_data = amns_data.amns_data()
+  #self.barometry = barometry.barometry()
   # etc. etc over all lower level IDSs
 
 
@@ -197,8 +255,9 @@ class IDSRoot():
  def setPulseCtx(self, ctx):
   self.expIdx = ctx
   self.connected = True
-  self.amns_data.setPulseCtx(ctx)
+  self.equilibrium.setPulseCtx(ctx)
   # Etc. etc for all other IDSs
+  #self.amns_data.setPulseCtx(ctx)
 
  def close(self):
   if (self.expIdx != -1):
@@ -267,86 +326,124 @@ class IDSRoot():
    raise ALException('ERROR calling ual_end_action().', status) 
   return status,timeList
 
-class IDSToplevel():
+class IDSStructure():
     _MAX_OCCURRENCES = None
-    @abc.abstractmethod
-    def __init__():
-        return
 
-    @abc.abstractclassmethod
     def getNodeType(cls):
-        return
+        raise NotImplementedError
 
-    @abc.abstractclassmethod
     def getMaxOccurrences(cls):
-        return
+        raise NotImplementedError
 
-    @abc.abstractmethod
     def __deepcopy__(self, memo):
-        return
+        raise NotImplementedError
 
-    @abc.abstractmethod
     def __copy__(self):
-        return
+        raise NotImplementedError
 
-    @abc.abstractmethod
-    def __init__(self):
-        return
+    def __init__(self, structure_name, structure_xml):
+        self._name = structure_name
+        self._children = []
+        for child in structure_xml.getchildren():
+            my_name = child.get('name')
+            print('IDSStructure name', my_name)
+            self._children.append(my_name)
+            my_data_type = child.get('data_type')
+            if my_data_type == 'structure':
+                child_hli = IDSStructure(my_name, child)
+                setattr(self, my_name, child_hli)
+            # 'class way'
+            #elif my_data_type in data_type_to_class:
+            #    setattr(self, my_name, data_type_to_class[my_data_type])
+            #    self._children.append(my_name)
+            # fill default way
+            elif my_data_type in data_type_to_default:
+                setattr(self, my_name, data_type_to_default[my_data_type])
+            else:
+                print('What to do? Unknown type!', my_data_type)
+                embed()
 
-    @abc.abstractmethod
+
     def initIDS(self):
-        return
+        raise NotImplementedError
 
-    @abc.abstractmethod
     def copyValues(self, ids):
-        """ Not sure what this should do"""
-        return
+        """ Not sure what this should do. Well, copy values of a structure!"""
+        raise NotImplementedError
 
-    @abc.abstractmethod
     def __str__(self, depth=0):
         """ Return a nice string representation """
-        return
-
-    def readHomogeneous(self, ctx):
-        """ Read the value of homogeneousTime """
         raise NotImplementedError
-        homogeneousTime = IDS_TIME_MODE_UNKNOWN
-        status, homogeneousTime = ull.ual_read_data(ctx, "ids_properties/homogeneous_time", "", INTEGER_DATA, 0)
-        if status != 0:
-            raise ALException('ERROR: homogeneous_time cannot be read.', status) 
-        return homogeneousTime
 
-    def read_data_dictionary_version(self, ctx):
-        """ Read the value of the DD version"""
-        raise NotImplementedError
-        data_dictionary_version = ''
-        status, data_dictionary_version = ull.ual_read_data_string(ctx, "ids_properties/version_put/data_dictionary", "", CHAR_DATA, 1)
-        if status != 0:
-            raise ALException('ERROR: data_dictionary_version cannot be read.', status) 
-        return data_dictionary_version
-
-    def readTime(self, ctx):
-        """ Read the value of /time, by convension the time coordinateversion"""
+    def readTime(self, occurrence):
         raise NotImplementedError
         time = []
+        path = None
+        if occurrence == 0:
+            path='equilibrium'
+        else:
+            path='equilibrium'+ '/' + str(occurrence)
+
+        status, ctx = ull.ual_begin_global_action(self._idx, path, READ_OP)
+        if status != 0:
+             raise ALException('Error calling ual_begin_global_action() in readTime() operation', status)
+    
         status, time = ull.ual_read_data_array(ctx, "time", "/time", DOUBLE_DATA, 1)
         if status != 0:
-            raise ALException('ERROR: TIME cannot be read.', status) 
+         raise ALException('ERROR: TIME cannot be read.', status) 
+        status = ull.ual_end_action(ctx)
+        if status != 0:
+         raise ALException('Error calling ual_end_action() in readTime() operation', status) 
         return time
 
+    def get(self, ctx, homogeneousTime):
+        strNodeRoot = "ids_properties/"
+        print('Getting all of', self._name)
+        for child_name in self._children:
+            child = getattr(self, child_name)
+            print('Trying to grab {!s} of type {!s}'.format(child_name, type(child)))
+            strNodePath = strNodeRoot + child_name
+            strTimeBasePath = ''
+            if isinstance(child, str):
+                status, data = ull.ual_read_data_string(ctx, strNodePath, strTimeBasePath, CHAR_DATA, 1)
+            elif isinstance(child, int):
+                status, data = ull.ual_read_data_scalar(ctx, strNodePath, strTimeBasePath, INTEGER_DATA)
+            else:
+                print('Unknown type {!s} for field {!s}! Not sure now..'.format(type(child), child_name))
+            if status == 0 and data is not None:
+                setattr(self, child_name, data)
+            else:
+                print('Warning! Unable to have grabbed simple type {!s}'.format(child_name))
 
-    @abc.abstractmethod
-    def get(self, occurrence=0):
+    def getSlice(self, time_requested, interpolation_method, occurrence=0):
         #Retrieve full IDS data from the open database.
-        return
+        raise NotImplementedError
 
-    @abc.abstractmethod
     def _getData(self, ctx, indexFrom, indexTo, homogeneousTime, nodePath, analyzeTime):
         """ A deeped way of getting data?? using 'traverser' whatever that is """
-        return
-    def put(self, occurrence=0):
-        #Store full IDS data to the open database.
         raise NotImplementedError
+
+    def put(self, ctx, homogeneousTime):
+        # Do not check if type is valid, just go for it
+        for child_name in self._children:
+            child = getattr(self, child_name)
+            if isinstance(child, IDSStructure):
+                print('Not yet putting IDS structures')
+                continue
+            elif not hli_utils.HLIUtils.isTypeValid(child, child_name, python_type_to_ual[type(child)]):
+                print('child {!s} of type {!s} has an invalid type, skip'.format(child_name, type(child)))
+                continue
+            if child is not None and child != '':
+                print('Trying to write {!s}'.format(child_name))
+                if isinstance(child, int):
+                    scalar_type = 1
+                    data = hli_utils.HLIUtils.isScalarFinite(child, scalar_type)
+                else:
+                    data = child
+                status = ull.ual_write_data(ctx, 'ids_properties/' + child_name, '', data)
+                if status != 0:
+                    raise ALException('Error writing field "{!s}"'.format(child_name))
+
 
     def putSlice(self, occurrence=0):
         #Store IDS data time slice to the open database.
@@ -354,13 +451,13 @@ class IDSToplevel():
 
     def deleteData(self, occurrence=0):
         #Delete full IDS data from the open database.
-        raise NotImplementedError
+        print('Not deleting data, everything is temporary atm!')
 
     def setExpIdx(self, idx):
         raise NotImplementedError
 
     def setPulseCtx(self, ctx):
-        raise NotImplementedError
+        self._idx = ctx
 
     def getPulseCtx(self):
         raise NotImplementedError
@@ -374,3 +471,124 @@ class IDSToplevel():
     def _getFromPath(self, dataPath,  occurrence, analyzeTime):
         #Retrieve partial IDS data without reading the full database content
         raise NotImplementedError
+
+class IDSToplevel(IDSStructure):
+    """ This is any IDS Structure which has ids_properties as child node
+
+    At minium, one should fill ids_properties/homogeneous_time
+    IF a quantity is filled, the coordinates of that quantity must be filled as well
+    """
+    def __init__(self, ids_name, ids_xml_element):
+        self.__name__ = ids_name
+        self._base_path = ids_name
+        self._idx = EMPTY_INT
+        self._children = []
+        print('Top level IDS name:', ids_name)
+        for child in ids_xml_element.getchildren():
+            my_name = child.get('name')
+            print(my_name)
+            if my_name != 'ids_properties':
+                # Only build ids_properties to KISS
+                continue
+            my_data_type = child.get('data_type')
+            self._children.append(my_name)
+            print('Initialized HL element', my_name)
+            if my_data_type == 'structure':
+                child_hli = IDSStructure(my_name, child)
+            else:
+                print('Unknown type ', my_data_type)
+                embed()
+            setattr(self, my_name, child_hli)
+        #self.initIDS()
+
+    def readHomogeneous(self, occurrence):
+        """ Read the value of homogeneousTime.
+
+        Returns:
+            0: IDS_TIME_MODE_HETEROGENEOUS; Dynamic nodes may be asynchronous, their timebase is located as indicted in the "Coordinates" column of the documentation
+            1: IDS_TIME_MODE_HOMOGENEOUS; All dynamic nodes are synchronous, their common timebase is the "time" node that is the child of the nearest parent IDS
+            2: IDS_TIME_MODE_INDEPENDENT; No dynamic node is filled in the IDS (dynamic nodes _will_ be skipped by the Access Layer)
+        """
+        homogeneousTime = IDS_TIME_MODE_UNKNOWN
+        path = self.__name__
+        if occurrence != 0:
+            path += '/' + str(occurrence)
+
+        status, ctx = ull.ual_begin_global_action(self._idx, path, READ_OP)
+        if status != 0:
+            raise ALException('Error calling ual_begin_global_action() in readHomogeneous() operation', status)
+
+        status, homogeneousTime = ull.ual_read_data(ctx, "ids_properties/homogeneous_time", "", INTEGER_DATA, 0)
+        if status != 0:
+            raise ALException('ERROR: homogeneous_time cannot be read.', status) 
+        status = ull.ual_end_action(ctx)
+        if status != 0:
+            raise ALException('Error calling ual_end_action() in readHomogeneous() operation', status) 
+        return homogeneousTime
+
+    def read_data_dictionary_version(self, occurrence):
+        data_dictionary_version = ''
+        path = self.__name__
+        if occurrence != 0:
+            path += '/' + str(occurrence)
+
+        status, ctx = ull.ual_begin_global_action(self._idx, path, READ_OP)
+        if status != 0:
+            raise ALException('Error calling ual_begin_global_action() in read_data_dictionary_version() operation', status)
+
+        status, data_dictionary_version = ull.ual_read_data_string(ctx, "ids_properties/version_put/data_dictionary", "", CHAR_DATA, 1)
+        if status != 0:
+            raise ALException('ERROR: data_dictionary_version cannot be read.', status) 
+        status = ull.ual_end_action(ctx)
+        if status != 0:
+            raise ALException('Error calling ual_end_action() in read_data_dictionary_version() operation', status) 
+        return data_dictionary_version
+
+    def get(self, occurrence=0):
+        path = None
+        if occurrence == 0:
+            path='equilibrium'
+        else:
+            path='equilibrium'+ '/' + str(occurrence)
+
+        homogeneousTime = self.readHomogeneous(occurrence)
+        if homogeneousTime == IDS_TIME_MODE_UNKNOWN:
+            print('Unknown time mode, not getting')
+            return
+        data_dictionary_version = self.read_data_dictionary_version(occurrence)
+
+        status, ctx = ull.ual_begin_global_action(self._idx, path, READ_OP)
+        if status != 0:
+          raise ALException('Error calling ual_begin_global_action() for equilibrium', status)
+
+        for child_name in self._children:
+            print('Trying to grab {!s}'.format(child_name))
+            child = getattr(self, child_name)
+            child.get(ctx, homogeneousTime)
+
+    def put(self, occurrence=0):
+        # Store full IDS data to the open database.
+        path = None
+        homogeneousTime = 2
+        if occurrence == 0:
+            path = self.__name__
+        else:
+            path = self.__name__ + '/' + str(occurrence)
+
+        homogeneousTime = self.ids_properties.homogeneous_time 
+        if homogeneousTime == IDS_TIME_MODE_UNKNOWN:
+            print("IDS equilibrium is found to be EMPTY (homogeneous_time undefined). PUT quits with no action.")
+            return
+        if homogeneousTime not in IDS_TIME_MODES:
+            raise ALException('ERROR: ids_properties.homogeneous_time should be set to IDS_TIME_MODE_HETEROGENEOUS, IDS_TIME_MODE_HOMOGENEOUS or IDS_TIME_MODE_INDEPENDENT.')
+        if homogeneousTime == IDS_TIME_MODE_HOMOGENEOUS and len(self.time)==0:
+            raise ALException('ERROR: the IDS%time vector of an homogeneous_time IDS must have a non-zero length.')
+        self.deleteData(occurrence)
+        status, ctx = ull.ual_begin_global_action(self._idx, path, WRITE_OP)
+        if status != 0:
+            raise ALException('Error calling ual_begin_global_action() for {!s}'.format(self.__name__, status))
+
+        for child_name in self._children:
+            child = getattr(self, child_name)
+            if isinstance(child, IDSStructure):
+                child.put(ctx, homogeneousTime)
