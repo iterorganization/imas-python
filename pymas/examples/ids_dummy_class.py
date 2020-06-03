7cc18fb871965cc76a7402045e2a7ccb00dec058
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2020-06-03 10:14:47 +0200

    Example working set in MDSPlus ArrayStructure

--- pymas/examples/ids_dummy_class.py
+++ pymas/examples/ids_dummy_class.py
@@ -21,7 +21,6 @@ if __name__ == '__main__':
 
     # Existing workflow example
     shot               = 130012
-    run_in             = 1
     #input_user_or_path = 'public'
     input_database     = 'iter'
     time_slice         = 200
@@ -42,18 +41,6 @@ if __name__ == '__main__':
     ACCESS_PULSE_0  = 40
     FORCE_CREATE_PULSE     =     ACCESS_PULSE_0+3
     input_user_or_path = 'vandepk'
-    # db_obj =  db_entry(backend, 'test', TEST_SHOT, TEST_RUN)
-    # Create a new entry
-    # This needs interaction with the ull
-    import ual_4_7_2._ual_lowlevel as ull
-    status, idx = ull.ual_begin_pulse_action(backend, shot, run_in, input_user_or_path, input_database, '3')
-    if status != 0:
-        raise Exception('Error calling ual_begin_pulse_action()')
-    status = ull.ual_open_pulse(idx, FORCE_CREATE_PULSE, '') 
-    if status != 0:
-        raise Exception('Error calling ual_open_pulse()')
-    db_ctx = idx 
-
 
     # Old API
     #import matplotlib.pyplot as plt
@@ -61,29 +48,25 @@ if __name__ == '__main__':
     style = 'new'
     if style == 'old':
         #import imas_3_28_1_ual_4_7_2_dev38 as imas
-        import imas
+        run_in = 0
+        try:
+            # Try loading Karels special library
+            import imas_3_28_1_ual_4_7_2_dev36 as imas
+        except ImportError:
+            # Load the regular IMAS library
+            import imas
 
         imas_entry = imas.ids(shot, run_in)
-        idx = imas_entry.create_env(input_user_or_path, input_database, '3')
-        ids = imas_entry.equilibrium
-        #ids.ids_properties.homogeneous_time = 2-
-        time = 0
-        ids.ids_properties.homogeneous_time = time
-        ids.setPulseCtx(db_ctx)
-        ids.put()
-        print('Temp database made')
-        print('Original ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
-        ids.ids_properties.homogeneous_time = 'foo'
-        print('Changed ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
-        ids.get()
-        print('After re-get ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
+        idx = imas_entry.create_env_backend(input_user_or_path, input_database, '3', backend)
     else:
         # New API
+        run_in = 1
         # 4.4.1 Initializing an empty IDS
         import pymas as imas
         idsdef_dir = os.path.join(os.path.dirname(__file__), '../../../imas-data-dictionary/')
         idsdef = os.path.join(idsdef_dir, 'IDSDef.xml')
         imas_entry = imas.ids(shot, run_in, xml_path=idsdef, verbosity=2)
+        idx = imas_entry.create_env_backend(input_user_or_path, input_database, '3', backend)
     # This should then support accessing an IDS using idsVar.<IDSname>.<IDSfield>
 
     # 5.5 Opening an existing Data Entry
@@ -103,23 +86,13 @@ if __name__ == '__main__':
     # Using python build-in management, or del var
 
     # 5.4 Creating a new data entry
-    if backend == MDSPLUS_BACKEND:
-        imas_entry.create_env(input_user_or_path, input_database, '3')
     # Put mandatory top-level entry
     ids = imas_entry.equilibrium
     #ids.ids_properties.homogeneous_time = 2
     time = 0
     ids.ids_properties.homogeneous_time = time
 
-    #thing = IDSStructure()
-    ids.setPulseCtx(db_ctx)
     ids.put()
-    #ids.ids_properties.homogeneous_time = 'crap'
-    print('Done putting')
-    print('Putting')
-
-    #ids.get(ctx=db_ctx, homogeneousTime=time)
-    print('Temp database made')
     print('Original ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
     ids.ids_properties.homogeneous_time = 9999
     print('Changed ids.ids_properties.homogeneous_time:', ids.ids_properties.homogeneous_time)
@@ -159,14 +132,18 @@ if __name__ == '__main__':
 
     ## Set beyond a struct array
     print('Pre put ids.time_slice:', ids.time_slice)
-    ids.time_slice.resize(1)
-    ids.time_slice[0].profiles_1d.psi = np.array([0, 0.5, 1])
+    ids.time_slice.resize(2)
+    ids.time_slice[0].profiles_1d.psi = np.array([0, 0.5, 1.5])
     ids.time_slice[0].time = 0.1
-    ids.put(verbosity=2)
+    ids.time_slice[1].time = 0.2
+    ids.put()
+    print('Post put ids.time_slice[0].time:', ids.time_slice[0].time)
     print('Post put ids.time_slice[0].profiles_1d.psi:', ids.time_slice[0].profiles_1d.psi)
     # Scramble
+    ids.time_slice[0].time = [-12344567]
     ids.time_slice[0].profiles_1d.psi = [-98767890]
     ids.get()
+    print('Post get ids.time_slice[0].time:', ids.time_slice[0].time)
     print('Post get ids.time_slice[0].profiles_1d.psi:', ids.time_slice[0].profiles_1d.psi)
 
     # time_slice.coordinate_system.grid.volume_element 2D_FLT
