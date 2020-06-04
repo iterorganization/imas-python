a1b5c4a1c980184955dc509638f06af93237e684
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2020-06-04 20:03:41 +0200

    Cleaned up setup.py with nicer no-UAL install

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 Packaging settings. Inspired by a minimal setup.py file, the Pandas cython build and the access-layer setup template
 """
 import os
+import shutil
 import argparse
 import sys
+from itertools import chain
 from codecs import open
 from subprocess import call
+
 from distutils.version import LooseVersion
 from distutils import sysconfig
 from setuptools import Command, find_packages, setup
@@ -15,6 +18,7 @@ import numpy #WHY?
 from distutils.command.build import build as _build
 
 
+this_dir = os.path.abspath(os.path.dirname(__file__)) # We need to know where we are for many things
 ### CYTHON COMPILATION MANGELING
 
 min_cython_ver = "0.29.16" #No idea, guess something
@@ -82,6 +86,15 @@ def maybe_cythonize(extensions, *args, **kwargs):
         nthreads = parsed.j
 
     kwargs["nthreads"] = nthreads
+    for ext in extensions:
+        if ext.name.startswith('ual_'):
+            version = ext.name[4:].split('.')[0]
+            if version != '0.0.0':
+                sources_prepared = prepare_ual_sources(force=fail_on_ual_fail)
+                if not sources_prepared:
+                    print('Not cythonizing', ext.name)
+                    extensions.remove(ext)
+
     #build_ext.render_templates(_pxifiles)
     return cythonize(extensions, *args, **kwargs)
 
@@ -110,91 +123,99 @@ class BuildExtWithoutPlatformSuffix(build_ext):
     filename = filename.replace(".dll", ".pyd")
     return filename
 
-### External variables with fallbacks
-config = {
-    'IMASPKGNAME'        : None,
-    'IMAS_VERSION_SAFE'  : None,
-    'UAL_VERSION_SAFE'   : None,
-    'IMAS_VERSION_SHORT' : None,
-    'UAL_VERSION_SHORT'  : None,
-    'IMASPKGVERSION'     : None,
-    'EXTRALINKARGS'      : None,
-    'EXTRACOMPILEARGS'   : None,
-    'LIBRARIES'          : None,
-    'OUTPUT_LIBRARY'     : None,
-    'LANGUAGE'           : None,
-}
+def prepare_ual_sources(force=False):
+    import git # Import git here, the user might not have it!
+
+    # This will probably _always_ depend on the UAL version.
+    # However, opposed to the original Python HLI, it does not
+    # depend on the IMAS DD version, as that is build dynamically in runtime
+
+    # Now we know which UAL target the user wants to install
+    # We need the actual source code (for now) so grab it from ITER
+    ual_repo_path = 'src/ual'
+    ual_repo_url = 'ssh://git@git.iter.org/imas/access-layer.git'
+
+    # Set up a bare repo and fetch the access-layer repository in it
+    os.makedirs(ual_repo_path, exist_ok=True)
+    try:
+        repo = git.Repo(ual_repo_path)
+    except git.exc.InvalidGitRepositoryError:
+        repo = git.Repo.init(ual_repo_path)
+
+    try:
+        origin = repo.remote()
+    except ValueError:
+        origin = repo.create_remote('origin', url=ual_repo_url)
+
+    try:
+        fetch_results = origin.fetch(UAL_VERSION)
+    except git.exc.GitCommandError:
+        msg = 'Could not find UAL_VERSION {!s}'.format(UAL_VERSION)
+        if force:
+            raise ValueError(msg)
+        else:
+            print(msg)
+            return
+    if len(fetch_results) > 1:
+        raise Exception('Git fetch returned multiple objects, should probably never happen')
+    elif len(fetch_results) == 0:
+        raise Exception('Git fetch returned no objects, should probably never happen')
+    else:
+        fetch_result = fetch_results[0]
+
+    # Check out remote files locally
+    head = repo.create_head(UAL_VERSION, fetch_result)
+    head.checkout()
+
+    # We should now have the Python HLI files, check
+    hli_src = os.path.join(this_dir, ual_repo_path, 'pythoninterface/src/imas')
+    if not os.path.isdir(hli_src):
+        raise Exception('Python interface src dir does not exist. Should have failed earlier')
 
 
-IMAS_PREFIX = os.getenv("IMAS_PREFIX")
-if not IMAS_PREFIX or not os.path.isdir(IMAS_PREFIX):
-    # Why is this a problem?
-    raise ValueError('IMAS_PREFIX is unset or is not a directory.')
+    # For the build, we need these
+    ual_cython_filelist = ['_ual_lowlevel.pyx', 'ual_defs.pxd', 'ual_lowlevel_interface.pxd']
+    # We need these in runtime, so check them here
+    filelist = ['imasdef.py', 'hli_utils.py', 'hli_exception.py']
 
-# Sanity checks, fallback to 0.0.0_0.0.0
-IMAS_VERSION = os.getenv("IMAS_VERSION")
-if not IMAS_VERSION or not len(IMAS_VERSION.split("."))>2:
-    print('IMAS_VERSION is unset or is not formatted as "0.0.0"')
-    IMAS_VERSION = "0.0.0"
-MAJOR = IMAS_VERSION.split(".")[0]
-MINOR = IMAS_VERSION.split(".")[1]
+    # Copy these files into the pymas directory
+    # TODO: This is a bit hacky, do this nicer
+    pymas_libs_dir = os.path.join(this_dir, 'pymas/_libs')
+    os.makedirs(pymas_libs_dir, exist_ok=True)
+    #if len(os.listdir(pymas_libs_dir)) != 0:
+        #raise Exception('pymas libs dir not empty, refusing to overwrite')
+    # Make _libs dir act as a python module
+    open(os.path.join(pymas_libs_dir, '__init__.py'), 'w').close()
 
-# Grab info from the command line
-parser = argparse.ArgumentParser()
-parser.add_argument("-j", type=int)
-parser.add_argument("--parallel", type=int)
-parsed, _ = parser.parse_known_args()
+    for file in chain(ual_cython_filelist, filelist):
+        path = os.path.join(hli_src, file)
+        if not os.path.isfile(path):
+            raise Exception('Could not find {!s}, should have failed earlier'.format(path))
+        else:
+            target_path = os.path.join(pymas_libs_dir, file)
+            # Patch some imports, they are different from regular Python HLI and pymas
+            # From PEP-8, absolute imports are preferred https://www.python.org/dev/peps/pep-0008/#id23
+            if file == '_ual_lowlevel.pyx':
+                with open(path, 'r') as old, open(target_path, 'w') as new:
+                    for line in old:
+                        if line == 'cimport ual_lowlevel_interface as ual\n':
+                            new.write('cimport pymas._libs.ual_lowlevel_interface as ual\n')
+                        elif line == 'from imasdef import *\n':
+                            new.write('from pymas._libs.imasdef import *\n')
+                        elif line == 'from hli_exception import ALException \n':
+                            new.write('from pymas._libs.hli_exception import ALException\n')
+                        else:
+                            new.write(line)
+            else:
+                shutil.copyfile(path, os.path.join(pymas_libs_dir, file))
 
-if '-' in IMAS_VERSION:
-    raise NotImplementedError
-else:
-    IMAS_VERSION_SAFE = IMAS_VERSION
+    return True
 
-UAL_VERSION = '4.7.2' # TODO: Do not hard code!
-if '-' in UAL_VERSION:
-    raise NotImplementedError
-else:
-    UAL_VERSION_SAFE = UAL_VERSION
-# Example
-# "s|@@IMASPKGNAME@@|imas_3_28_1_ual_4_7_2_dev38|g" \
-# "s|@@IMAS_VERSION_SAFE@@|3.28.1|g" \
-# "s|@@UAL_VERSION_SAFE@@|4.7.2|g" \
-# "s|@@IMAS_VERSION_SHORT@@|3.28.1|g" \
-# "s|@@UAL_VERSION_SHORT@@|4.7.2|g" \
-# "s|@@IMASPKGVERSION@@|4.7.2.dev38+g2cc2ab8cdirty|g" \
-# 's|@@EXTRALINKARGS@@||g' \
-# 's|@@EXTRACOMPILEARGS@@|"-D__USE_XOPEN2K8"|g' \
-# 's|@@LIBRARIES@@|"imas"|g' \
-# 's|@@OUTPUT_LIBRARY@@||g' \
-# 's|@@LANGUAGE@@|"c"|g' \
-
-this_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
-LANGUAGE = 'c' # Not sure when this is not true
-LIBRARIES = 'imas' # We just need the IMAS library
-#EXTRALINKARGS = '' # This is machine-specific
-#EXTRACOMPILEARGS = '' # This is machine-specific
-
-# This will probably _always_depend on the UAL version. However, opposed to the original Python HLI, it does not depend on the IMAS DD version, as that is build dynamically in runtime
-pxd_path = os.path.join(this_dir, 'pymas/_libs')
-print(os.listdir(pxd_path))
-ext_module_name = "ual_{!s}._ual_lowlevel".format(UAL_VERSION_SAFE.replace('.', '_'))
-ext_module = Extension(
-  name = ext_module_name,
-  sources = [ "pymas/_libs/_ual_lowlevel.pyx" ], # As we're targetting a single UAL (For not), the sources are easy to find!
-  language = LANGUAGE,
-  library_dirs = [ IMAS_PREFIX + "/lib" ],
-  libraries = [ LIBRARIES ],
-  #extra_link_args = [ EXTRALINKARGS ],
-  #extra_compile_args = [ EXTRACOMPILEARGS ],
-  include_dirs=[ IMAS_PREFIX + "/include", numpy.get_include(), pxd_path],
-)
-
 
 class RunTests(Command):
     """Run all tests."""
@@ -211,6 +232,71 @@ class RunTests(Command):
         """Run all tests!"""
         errno = call(['py.test'])
         raise SystemExit(errno)
+
+# Try to grab all necessary environment variables.
+# IMAS_PREFIX points to the directory all IMAS components live in
+IMAS_PREFIX = os.getenv("IMAS_PREFIX")
+if not IMAS_PREFIX or not os.path.isdir(IMAS_PREFIX):
+    print('IMAS_PREFIX is unset or is not a directory. Points to {!s}. Will not build UAL!'.format(IMAS_PREFIX))
+    IMAS_PREFIX = '0.0.0'
+
+# Legacy code, we do not need an explicit IMAS_VERSION.
+# What is more important, we need the UAL_VERSION we want to link
+# to
+## Sanity checks, fallback to 0.0.0_0.0.0
+#IMAS_VERSION = os.getenv("IMAS_VERSION")
+#if not IMAS_VERSION or not len(IMAS_VERSION.split("."))>2:
+#    print('IMAS_VERSION is unset or is not formatted as "0.0.0"')
+#    IMAS_VERSION = "0.0.0"
+#MAJOR = IMAS_VERSION.split(".")[0]
+#MINOR = IMAS_VERSION.split(".")[1]
+
+# Grab the UAL_VERSION to build against
+UAL_VERSION = os.getenv("UAL_VERSION")
+if not UAL_VERSION:
+    print('UAL_VERSION is unset. Will not build UAL!')
+    UAL_VERSION = '0.0.0'
+
+# Safeify the UAL_VERSION
+if '-' in UAL_VERSION:
+    raise NotImplementedError
+else:
+    UAL_VERSION_SAFE = UAL_VERSION.replace('.', '_')
+
+# We need source files of the Python HLI UAL library
+# to link our build against, the version is grabbed from
+# the environment, and they are saved as syubdirectory of
+# pymas
+pxd_path = os.path.join(this_dir, 'pymas/_libs')
+
+ext_module_name = "ual_{!s}._ual_lowlevel".format(UAL_VERSION_SAFE)
+
+LANGUAGE = 'c' # Not sure when this is not true
+LIBRARIES = 'imas' # We just need the IMAS library
+#EXTRALINKARGS = '' # This is machine-specific ignore for now
+#EXTRACOMPILEARGS = '' # This is machine-specific ignore for now
+
+ext_module = Extension(
+  name = ext_module_name,
+  sources = [ "pymas/_libs/_ual_lowlevel.pyx" ], # As these files are copied, easy to find!
+  language = LANGUAGE,
+  library_dirs = [ IMAS_PREFIX + "/lib" ],
+  libraries = [ LIBRARIES ],
+  #extra_link_args = [ EXTRALINKARGS ],
+  #extra_compile_args = [ EXTRACOMPILEARGS ],
+  include_dirs=[ IMAS_PREFIX + "/include", numpy.get_include(), pxd_path],
+)
+ext_modules = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--build-ual", action='store_true')
+args, leftovers = parser.parse_known_args()
+fail_on_ual_fail = args.build_ual
+
+if ext_module is not None:
+    ext_modules.append(ext_module)
+else:
+    print('Cannot build UAL')
 
 setup(
     name = 'pymas',
@@ -234,6 +320,6 @@ setup(
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov'],
     },
-    ext_modules = maybe_cythonize([ext_module]),
+    ext_modules = maybe_cythonize(ext_modules),
     cmdclass = {'test': RunTests, "build_ext": build_ext, "build": _build },
 )
