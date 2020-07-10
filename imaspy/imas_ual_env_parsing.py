d9d36076358f512d123effd74e5b029e21f80995
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2020-07-10 12:24:33 +0200

    Add file to parse package names

---
+++ imaspy/imas_ual_env_parsing.py
@@ -0,0 +1,16 @@
+def parse_UAL_version_string(string):
+    if '-' in string:
+        ual_patch_version, micropatch = string.split('-', 1)
+        steps_from_version, commitspec = micropatch.split('-', 2)
+        ual_commit = commitspec[1:]
+    else:
+        ual_patch_version = string
+        steps_from_version = '0'
+        ual_commit = string
+    return ual_patch_version, steps_from_version, ual_commit
+
+def sanitise_UAL_patch_version(ual_patch_version):
+    return ual_patch_version.replace('.', '_')
+
+def build_UAL_package_name(safe_ual_patch_version, ual_commit):
+    return "ual_{!s}_{!s}._ual_lowlevel".format(safe_ual_patch_version, ual_commit)
