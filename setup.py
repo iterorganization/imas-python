c88f639097707db1f2b321de1f81d3e5d00b965c
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2020-07-21 16:02:05 +0200

    Modern PyPA style setup.py

--- setup.py
+++ setup.py
@@ -1,104 +1,143 @@
-#! /usr/bin/env python
 """
-Packaging settings. Inspired by a minimal setup.py file, the Pandas cython build and the access-layer setup template
-"""
-import os
-import shutil
-import argparse
-import sys
-from itertools import chain
-from codecs import open
-from subprocess import call
+Packaging settings. Inspired by a minimal setup.py file, the Pandas cython build
+and the access-layer setup template.
 
+The reference method of installing is determined by the
+[Python Package Authority](https://packaging.python.org/tutorials/installing-packages/)
+of which a summary and advanced explanation is on the [IMASPy wiki](https://gitlab.com/imaspy-dev/imaspy/-/wikis/installing)
+
+The installable IMASPy package tries to follow in the following order:
+- The style guide for Python code [PEP8](https://www.python.org/dev/peps/pep-0008/)
+- The PyPA guide on packaging projects https://packaging.python.org/guides/distributing-packages-using-setuptools/#distributing-packages
+- The PyPA tool recommendations https://packaging.python.org/guides/tool-recommendations/, specifically:
+  * Installing: [pip](https://pip.pypa.io/en/stable/)
+  * Environment management: [venv](https://docs.python.org/3/library/venv.html)
+  * Dependency managamenet: [pip-tools](https://github.com/jazzband/pip-tools)
+  * Packaging source distributions: [setuptools](https://setuptools.readthedocs.io/)
+  * Packaging built distribitions: [wheels](https://pythonwheels.com/)
+"""
+# Set up 'fancy logging' to display messages to the user
+import logging
+import imaspy
+imaspy_logger = logging.getLogger('imaspy')
+logger = imaspy_logger
+logger.setLevel(logging.INFO)
+
+logger.info("pyproject.toml support got added in pip 10. Assuming it is available")
+
+###
+# HANDLE USER ENVIRONMENT
+###
+import argparse
+import os
+from imaspy.imas_ual_env_parsing import parse_UAL_version_string, sanitise_UAL_patch_version, build_UAL_package_name
+this_dir = os.path.abspath(os.path.dirname(__file__)) # We need to know where we are for many things
+parser = argparse.ArgumentParser()
+parser.add_argument("--build-ual", action='store_true')
+args, leftovers = parser.parse_known_args()
+fail_on_ual_fail = args.build_ual
+
+# Now that the environment is defined, import the rest of the needed packages
+import sys
+import shutil
+from itertools import chain
+from subprocess import call
+import logging
 from distutils.version import LooseVersion
 from distutils import sysconfig
-from setuptools import Command, find_packages, setup
+from setuptools import Command, find_packages, setup, Extension
 
-import numpy #WHY?
-from distutils.command.build import build as _build
-from imaspy.imas_ual_env_parsing import parse_UAL_version_string, sanitise_UAL_patch_version, build_UAL_package_name
 
-this_dir = os.path.abspath(os.path.dirname(__file__)) # We need to know where we are for many things
-### CYTHON COMPILATION MANGELING
 
-min_cython_ver = "0.29.16" #No idea, guess something
+# Try to grab all necessary environment variables.
+# IMAS_PREFIX points to the directory all IMAS components live in
+IMAS_PREFIX = os.getenv("IMAS_PREFIX")
+if not IMAS_PREFIX or not os.path.isdir(IMAS_PREFIX):
+    logger.warning('IMAS_PREFIX is unset or is not a directory. Points to {!s}. Will not build UAL!'.format(IMAS_PREFIX))
+    IMAS_PREFIX = '0.0.0'
 
-try:
-    import Cython
+# Legacy code, we do not need an explicit IMAS_VERSION.
+# What is more important, we need the UAL_VERSION we want to link
+# to
+## Sanity checks, fallback to 0.0.0_0.0.0
+#IMAS_VERSION = os.getenv("IMAS_VERSION")
+#if not IMAS_VERSION or not len(IMAS_VERSION.split("."))>2:
+#    print('IMAS_VERSION is unset or is not formatted as "0.0.0"')
+#    IMAS_VERSION = "0.0.0"
+#MAJOR = IMAS_VERSION.split(".")[0]
+#MINOR = IMAS_VERSION.split(".")[1]
 
-    _CYTHON_VERSION = Cython.__version__
-    from Cython.Build import cythonize
+# Grab the UAL_VERSION to build against
+UAL_VERSION = os.getenv("UAL_VERSION")
+if not UAL_VERSION:
+    logger.warning('UAL_VERSION is unset. Will not build UAL!')
+    UAL_VERSION = '0.0.0'
 
-    _CYTHON_INSTALLED = _CYTHON_VERSION >= LooseVersion(min_cython_ver)
-except ImportError:
-    raise
-    _CYTHON_VERSION = None
-    _CYTHON_INSTALLED = False
-    cythonize = lambda x, *args, **kwargs: x  # dummy func
+ual_patch_version, steps_from_version, ual_commit = parse_UAL_version_string(UAL_VERSION)
 
-# The import of Extension must be after the import of Cython, otherwise
-# we do not get the appropriately patched class.
-# See https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html # noqa
-from distutils.extension import Extension
-from distutils.command.build import build
+safe_ual_patch_version = sanitise_UAL_patch_version(ual_patch_version)
+ext_module_name = build_UAL_package_name(safe_ual_patch_version, ual_commit)
 
-if _CYTHON_INSTALLED:
-    #from Cython.Distutils.old_build_ext import old_build_ext as _build_ext # Do not use the old build_ext
-    from Cython.Distutils import build_ext as _build_ext
+# We need source files of the Python HLI UAL library
+# to link our build against, the version is grabbed from
+# the environment, and they are saved as syubdirectory of
+# imaspy
+pxd_path = os.path.join(this_dir, 'imaspy/_libs')
 
-    cython = True
-    from Cython import Tempita as tempita
-else:
-    from distutils.command.build_ext import build_ext as _build_ext
+LANGUAGE = 'c' # Not sure when this is not true
+LIBRARIES = 'imas' # We just need the IMAS library
+#EXTRALINKARGS = '' # This is machine-specific ignore for now
+#EXTRACOMPILEARGS = '' # This is machine-specific ignore for now
 
-    cython = False
-build_ext = _build_ext # No monkey patching needed
+###
+# Set up Cython build integration
+###
+### CYTHON COMPILATION ENVIRONMENT
 
-def maybe_cythonize(extensions, *args, **kwargs):
-    """
-    Render tempita templates before calling cythonize. This is skipped for
-    * clean
-    * sdist
-    """
-    if "clean" in sys.argv or "sdist" in sys.argv:
-        # See https://github.com/cython/cython/issues/1495
-        return extensions
+# From https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#multiple-cython-files-in-a-package
+USE_CYTHON = True
+cython_like_ext = '.pyx' if USE_CYTHON else '.c'
 
-    elif not cython:
-        # GH#28836 raise a helfpul error message
-        if _CYTHON_VERSION:
-            raise RuntimeError(
-                f"Cannot cythonize with old Cython version ({_CYTHON_VERSION} "
-                f"installed, needs {min_cython_ver})"
-            )
-        raise RuntimeError("Cannot cythonize without Cython installed.")
+###
+# Build extention list
+###
+extensions = []
 
-    # reuse any parallel arguments provided for compilation to cythonize
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-j", type=int)
-    parser.add_argument("--parallel", type=int)
-    parsed, _ = parser.parse_known_args()
+import numpy as np
+ual_module = Extension(
+  name = ext_module_name,
+  sources = [ "imaspy/_libs/_ual_lowlevel.pyx" ], # As these files are copied, easy to find!
+  language = LANGUAGE,
+  library_dirs = [ IMAS_PREFIX + "/lib" ],
+  libraries = [ LIBRARIES ],
+  #extra_link_args = [ EXTRALINKARGS ],
+  #extra_compile_args = [ EXTRACOMPILEARGS ],
+  include_dirs=[ IMAS_PREFIX + "/include", np.get_include(), pxd_path],
+)
+extensions.append(ual_module)
 
-    nthreads = 0
-    if parsed.parallel:
-        nthreads = parsed.parallel
-    elif parsed.j:
-        nthreads = parsed.j
+###
+# Set up Cython compilation (or not)
+###
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in ('.pyx', '.py'):
+                if extension.language == 'c++':
+                    ext = '.cpp'
+                else:
+                    ext = '.c'
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
 
-    kwargs["nthreads"] = nthreads
-    for ext in extensions:
-        if ext.name.startswith('ual_'):
-            version = ext.name[4:].split('.')[0]
-            if version != '0.0.0':
-                sources_prepared = prepare_ual_sources(force=fail_on_ual_fail)
-                if not sources_prepared:
-                    print('Not cythonizing', ext.name)
-                    extensions.remove(ext)
+extensions = no_cythonize(extensions)
 
-    #build_ext.render_templates(_pxifiles)
-    return cythonize(extensions, *args, **kwargs)
 
-### IMAS-style package names
+### IMAS-style package names (Not use right now)
 def get_ext_filename_without_platform_suffix(filename):
   """ Remove specific system filename extension """
   name, ext = os.path.splitext(filename)
@@ -115,19 +154,12 @@ def get_ext_filename_without_platform_suffix(filename):
   else:
     return name[:idx] + ext
 
-class BuildExtWithoutPlatformSuffix(build_ext):
-  def get_ext_filename(self, ext_name):
-    filename = super().get_ext_filename(ext_name)
-    filename = get_ext_filename_without_platform_suffix(filename)
-    # For Windows MINGW, replace extension ".dll" by ".pyd"
-    filename = filename.replace(".dll", ".pyd")
-    return filename
-
 def prepare_ual_sources(force=False):
+    """ Use gitpython to grab AL sources from ITER repository """
     try:
         import git # Import git here, the user might not have it!
     except ModuleNotFoundError:
-        print("Could not find 'git' module, try 'pip install --user gitpython'. Will not build UAL!")
+        logger.warning("Could not find 'git' module, try 'pip install gitpython'. Will not build AL!")
         return False
 
     # This will probably _always_ depend on the UAL version.
@@ -145,16 +177,16 @@ def prepare_ual_sources(force=False):
         repo = git.Repo(ual_repo_path)
     except git.exc.InvalidGitRepositoryError:
         repo = git.Repo.init(ual_repo_path)
-    print("Set up local git repository {!s}".format(repo))
+    logger.info("Set up local git repository {!s}".format(repo))
 
     try:
         origin = repo.remote()
     except ValueError:
         origin = repo.create_remote('origin', url=ual_repo_url)
-    print("Set up remote '{!s}' linking to '{!s}'".format(origin, origin.url))
+    logger.info("Set up remote '{!s}' linking to '{!s}'".format(origin, origin.url))
 
     origin.fetch('--tags')
-    print("Remote tags fetched")
+    logger.info("Remote tags fetched")
 
     # First check if we have the commit already
     head = None
@@ -163,12 +195,12 @@ def prepare_ual_sources(force=False):
             head = head
 
     if head is None:
-        print("Commit '{!s}' not found locally, trying remote".format(ual_commit))
+        logger.info("Commit '{!s}' not found locally, trying remote".format(ual_commit))
         # If we do not have the commit, fetch master
         #refspec='remotes/origin/' + ual_commit + ':' + ual_commit
         #refspec = ual_commit + ':' + ual_commit
         refspec = 'master'
-        print('Fetching refspec {!s}'.format(refspec))
+        logger.info('Fetching refspec {!s}'.format(refspec))
 
         fetch_results = origin.fetch(refspec=refspec)
         if len(fetch_results) == 1:
@@ -233,84 +265,6 @@ with open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as file:
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-class RunTests(Command):
-    """Run all tests."""
-    description = 'run tests'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        """Run all tests!"""
-        errno = call(['py.test'])
-        raise SystemExit(errno)
-
-# Try to grab all necessary environment variables.
-# IMAS_PREFIX points to the directory all IMAS components live in
-IMAS_PREFIX = os.getenv("IMAS_PREFIX")
-if not IMAS_PREFIX or not os.path.isdir(IMAS_PREFIX):
-    print('IMAS_PREFIX is unset or is not a directory. Points to {!s}. Will not build UAL!'.format(IMAS_PREFIX))
-    IMAS_PREFIX = '0.0.0'
-
-# Legacy code, we do not need an explicit IMAS_VERSION.
-# What is more important, we need the UAL_VERSION we want to link
-# to
-## Sanity checks, fallback to 0.0.0_0.0.0
-#IMAS_VERSION = os.getenv("IMAS_VERSION")
-#if not IMAS_VERSION or not len(IMAS_VERSION.split("."))>2:
-#    print('IMAS_VERSION is unset or is not formatted as "0.0.0"')
-#    IMAS_VERSION = "0.0.0"
-#MAJOR = IMAS_VERSION.split(".")[0]
-#MINOR = IMAS_VERSION.split(".")[1]
-
-# Grab the UAL_VERSION to build against
-UAL_VERSION = os.getenv("UAL_VERSION")
-if not UAL_VERSION:
-    print('UAL_VERSION is unset. Will not build UAL!')
-    UAL_VERSION = '0.0.0'
-
-ual_patch_version, steps_from_version, ual_commit = parse_UAL_version_string(UAL_VERSION)
-
-safe_ual_patch_version = sanitise_UAL_patch_version(ual_patch_version)
-ext_module_name = build_UAL_package_name(safe_ual_patch_version, ual_commit)
-
-# We need source files of the Python HLI UAL library
-# to link our build against, the version is grabbed from
-# the environment, and they are saved as syubdirectory of
-# imaspy
-pxd_path = os.path.join(this_dir, 'imaspy/_libs')
-
-LANGUAGE = 'c' # Not sure when this is not true
-LIBRARIES = 'imas' # We just need the IMAS library
-#EXTRALINKARGS = '' # This is machine-specific ignore for now
-#EXTRACOMPILEARGS = '' # This is machine-specific ignore for now
-
-ext_module = Extension(
-  name = ext_module_name,
-  sources = [ "imaspy/_libs/_ual_lowlevel.pyx" ], # As these files are copied, easy to find!
-  language = LANGUAGE,
-  library_dirs = [ IMAS_PREFIX + "/lib" ],
-  libraries = [ LIBRARIES ],
-  #extra_link_args = [ EXTRALINKARGS ],
-  #extra_compile_args = [ EXTRACOMPILEARGS ],
-  include_dirs=[ IMAS_PREFIX + "/include", numpy.get_include(), pxd_path],
-)
-ext_modules = []
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--build-ual", action='store_true')
-args, leftovers = parser.parse_known_args()
-fail_on_ual_fail = args.build_ual
-
-if ext_module is not None:
-    ext_modules.append(ext_module)
-else:
-    print('Cannot build UAL')
-
 setup(
     name = 'imaspy',
     version = '0.0.1',
@@ -329,10 +283,10 @@ setup(
     ],
     packages = find_packages(exclude=['docs', 'tests*']),
     install_requires = requirements,
-    setup_requires = [ "numpy", "cython" ],
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov'],
     },
-    ext_modules = maybe_cythonize(ext_modules),
-    cmdclass = {'test': RunTests, "build_ext": build_ext, "build": _build },
+    ext_modules = extensions,
+    cmdclass = {'test': RunTests},
+                #, "build_ext": build_ext},
 )
