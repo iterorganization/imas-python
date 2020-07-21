198a0aa4422afa1d73b624fba2c62cdb11212a2d
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2020-07-21 16:00:47 +0200

    Use different package name for release UAL versions

--- imaspy/imas_ual_env_parsing.py
+++ imaspy/imas_ual_env_parsing.py
@@ -6,11 +6,16 @@ def parse_UAL_version_string(string):
     else:
         ual_patch_version = string
         steps_from_version = '0'
-        ual_commit = string
+        ual_commit = ''
     return ual_patch_version, steps_from_version, ual_commit
 
 def sanitise_UAL_patch_version(ual_patch_version):
     return ual_patch_version.replace('.', '_')
 
 def build_UAL_package_name(safe_ual_patch_version, ual_commit):
-    return "ual_{!s}_{!s}._ual_lowlevel".format(safe_ual_patch_version, ual_commit)
+    if ual_commit == '':
+        # Release package
+        return "ual_{!s}._ual_lowlevel".format(safe_ual_patch_version)
+    else:
+        # 'dev' or 'in-between' package
+        return "ual_{!s}_{!s}._ual_lowlevel".format(safe_ual_patch_version, ual_commit)
