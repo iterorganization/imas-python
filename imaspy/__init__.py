2bda96a1b6501e2959f16c55713b5812708fd440
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2020-11-16 15:51:01 +0100

    Copy version from setuptools SCM to more general __version__

--- imaspy/__init__.py
+++ imaspy/__init__.py
@@ -12,6 +12,14 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with IMASPy.  If not, see <https://www.gnu.org/licenses/>.
+
+from .version import version as __version_from_scm__
+
+# Set the package version equal to the one grabbed from the
+# Source Management System
+# For git-desribe (this repository) it is based on tagged commits
+__version__ = __version_from_scm__
+
 from imaspy import (
     setup_logging,
     imas_ual_env_parsing,
