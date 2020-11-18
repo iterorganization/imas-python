7051c0432582b947ae94ff614d90683871b8ecfa
Daan van Vugt <daanvanvugt@gmail.com>
2020-11-18 10:57:55 +0100

    pull AL and DD from ITER

---
+++ containers/arch/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git clone ssh://git@git.iter.org/imas/data-dictionary.git
+cd data-dictionary
+git checkout saxon-cmd
+
+cd ..
+git clone ssh://git@git.iter.org/imas/access-layer.git
+cd access-layer
+git checkout saxon-cmd
