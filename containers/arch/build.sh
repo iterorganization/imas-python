9a00d41c8dd3a09ef333c3073a49125662cd3989
Daan van Vugt <daanvanvugt@gmail.com>
2020-11-19 11:03:33 +0100

    Build IMAS and IMASPy

--- containers/arch/build.sh
+++ containers/arch/build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 git clone ssh://git@git.iter.org/imas/data-dictionary.git
-cd data-dictionary
-git checkout saxon-cmd
 
-cd ..
 git clone ssh://git@git.iter.org/imas/access-layer.git
-cd access-layer
-git checkout develop
