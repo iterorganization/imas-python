d2d1bee0c5286b7fc762027637c7d1941b5bf93c
Daan van Vugt <daanvanvugt@gmail.com>
2020-11-24 16:12:42 +0100

    Clone imaspy in build.sh

--- containers/arch/build.sh
+++ containers/arch/build.sh
@@ -3,4 +3,6 @@ git clone ssh://git@git.iter.org/imas/data-dictionary.git
 
 git clone ssh://git@git.iter.org/imas/access-layer.git
 
+git clone ssh://git@gitlab.com/klimex/imaspy.git
+
 docker build .
