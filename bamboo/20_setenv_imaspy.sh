1fb17280105e74ac18cc999b720135bf9e586444
Daan van Vugt <daanvanvugt@gmail.com>
2020-12-19 10:28:04 +0100

    Remove debug info

--- bamboo/20_setenv_imaspy.sh
+++ bamboo/20_setenv_imaspy.sh
@@ -2,9 +2,6 @@
 # Set up IMASPy Python environment
 export PYTHON_INSTALL_DIR=${PYTHON_INSTALL_DIR:-`pwd`/install}
 export build_deps=$(cat pyproject.toml | grep requires | cut -d'=' -f2- | tr -d ,\"\' | sed "s/^ \[//" | sed "s/\]$//")
-cd `pwd`
-pwd
-ls
 export run_dep_files=$(find 'requirements_*' -maxdepth 1 -and -not -name "*test*" -and -not -name "*backends_al*" -and -not -name "*examples*")
 export test_deps=$(find requirements_* -maxdepth 1 -and -name "*test*")
 
