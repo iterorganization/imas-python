3614719ff87e925f8419daf1ff56ae8fe4130c1a
Daan van Vugt <daanvanvugt@gmail.com>
2020-12-21 14:31:31 +0100

    Fix iter ci pip install

--- bamboo/30_build_imaspy.sh
+++ bamboo/30_build_imaspy.sh
@@ -2,7 +2,7 @@
 # Build IMASPy dists and wheel
 
 export build_deps=$(cat pyproject.toml | grep requires | cut -d'=' -f2- | tr -d ,\"\' | sed "s/^ \[//" | sed "s/\]$//")
-export test_deps=$(find requirements_* -maxdepth 1 -and -name "*test*")
+export test_deps=$(find . -maxdepth 1 -and -name "requirements_test*")
 
 # Pip install build dependencies
 $PIP install --user --ignore-installed pip setuptools
@@ -10,7 +10,7 @@ $PIP install --upgrade $build_deps
 
 # install rundeps into local install folder
 # Install run dependencies in local install folder
-export run_dep_files=$(find 'requirements_*' -maxdepth 1 -and -not -name "*test*" -and -not -name "*backends_al*" -and -not -name "*examples*")
+export run_dep_files=$(find . -maxdepth 1 -and -not -name "*test*" -and -not -name "*backends_al*" -and -not -name "*examples*" -and -name 'requirements_*')
 for file in $run_dep_files; do
     echo Installing $file
     $PIP install -r $file
