c7a844ad929d2da8f286ae55eb9f9f85602167d4
Daan van Vugt <daan@vanvugt.me>
2020-12-21 14:24:19 +0100

    move build and run deps to build script

--- bamboo/30_build_imaspy.sh
+++ bamboo/30_build_imaspy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Build IMASPy dists and wheel
 
+export build_deps=$(cat pyproject.toml | grep requires | cut -d'=' -f2- | tr -d ,\"\' | sed "s/^ \[//" | sed "s/\]$//")
+export test_deps=$(find requirements_* -maxdepth 1 -and -name "*test*")
 
 # Pip install build dependencies
 $PIP install --user --ignore-installed pip setuptools
@@ -8,6 +10,7 @@ $PIP install --upgrade $build_deps
 
 # install rundeps into local install folder
 # Install run dependencies in local install folder
+export run_dep_files=$(find 'requirements_*' -maxdepth 1 -and -not -name "*test*" -and -not -name "*backends_al*" -and -not -name "*examples*")
 for file in $run_dep_files; do
     echo Installing $file
     $PIP install -r $file
