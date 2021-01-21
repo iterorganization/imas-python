b734a5ce398f2cbd1cbbe434cffc98a082dbfabd
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-01-21 20:02:34 +0100

    Add shell script to build HTML pages

---
+++ bamboo/80_build_imaspy_docs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Set up ITER modules environment
+set -e
+my_dir=$(dirname $0)
+. $my_dir/00_setenv_modules.sh
+. $my_dir/10_setenv_imas_monorepo.sh
+. $my_dir/20_setenv_imaspy.sh
+. $my_dir/30_build_imaspy.sh
+. $my_dir/40_install_imaspy.sh
+
+make docs
