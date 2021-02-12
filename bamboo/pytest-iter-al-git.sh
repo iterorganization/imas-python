c77b7f9b92e9827efc4a3d63dc4a60d539ecee38
Olivier Hoenen <Olivier.Hoenen@iter.org>
2021-02-12 10:33:13 +0100

    Fix typo

--- bamboo/pytest-iter-al-git.sh
+++ bamboo/pytest-iter-al-git.sh
@@ -3,7 +3,7 @@
 set -e
 my_dir=$(dirname $0)
 . $my_dir/00_setenv_modules.sh
-. $my_dir/10_build_imas_git.sh ${1:=develop}
+. $my_dir/10_build_imas_git.sh ${1:-develop}
 . $my_dir/20_setenv_imaspy.sh
 . $my_dir/30_build_imaspy.sh
 . $my_dir/40_install_imaspy.sh
