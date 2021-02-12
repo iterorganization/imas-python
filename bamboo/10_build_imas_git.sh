686acffdf09b2c33efa748bf96a2a434694959ff
Olivier Hoenen <Olivier.Hoenen@iter.org>
2021-02-12 10:33:40 +0100

    Fix env and check existence of DD qnd AL repo

--- bamboo/10_build_imas_git.sh
+++ bamboo/10_build_imas_git.sh
@@ -7,16 +7,19 @@ export PYTHON_INSTALL_DIR=${PYTHON_INSTALL_DIR:-`pwd`/install}
 export PYTHONUSERBASE=$PYTHON_INSTALL_DIR
 
 # Use the parts of IMAS
-module load GCC/10.2.0
-module load Python/3.8.6-GCCcore-10.2.0
-module load Saxon-HE
+module load GCC/9.3.0
+module load Python/3.8.2-GCCcore-9.3.0
+module load Saxon-HE/9.9.1.7-Java-13
 module load MDSplus/7.96.12-GCCcore-9.3.0
-module load MDSplus-Java
+module load MDSplus-Java/7.96.12-GCCcore-9.3.0-Java-13
 module load HDF5/1.12.0-gompi-2020a
-module load Boost
+module load Boost/1.72.0-GCCcore-9.3.0-no_mpi
 module list
 
 # install a DD locally
+if [ ! -d data-dictionary ]; then
+    git clone ssh://git@git.iter.org/imas/data-dictionary.git
+fi
 cd data-dictionary
 # use the latest tagged version
 export IMAS_VERSION=`git tag | sort -V | tail -n 1`
@@ -24,8 +27,12 @@ git checkout "$IMAS_VERSION"
 #git clean -xf
 make
 
+cd ..
 # INSTALL the access layer locally (this might take a while)
-cd ../access-layer
+if [ ! -d access-layer ]; then
+    git clone ssh://git@git.iter.org/imas/access-layer.git
+fi
+cd access-layer
 git checkout "$1"
 #git clean -xf
 if [ ! -L xml ]; then
@@ -45,13 +52,13 @@ export C_INCLUDE_PATH=`pwd`/lowlevel:${C_INCLUDE_PATH:=}
 export LD_LIBRARY_PATH=`pwd`/lowlevel:${LD_LIBRARY_PATH:=}
 
 cd lowlevel
-make
+make -j
 cd ..
 
 pip install --user numpy cython
 
 cd pythoninterface
-make
+make -j
 sed -e 's/imas_[^/."]*/imas/g' -i package/setup.py
 
 # install locally
