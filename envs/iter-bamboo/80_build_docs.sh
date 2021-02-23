83e2d431f4d427d26c6337afeb27f27a0653ebec
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-02-23 09:56:53 +0100

    Use IMAS git env for docs build as well

---
+++ envs/iter-bamboo/80_build_docs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Build HTML pages from source
+set -e
+my_dir=$(dirname $0)
+
+make docs
--- envs/iter-bamboo/build_imaspy_docs.sh
+++ envs/iter-bamboo/build_imaspy_docs.sh
@@ -2,10 +2,12 @@
 # Set up ITER modules environment
 set -e
 my_dir=$(dirname $0)
+
 . $my_dir/00_setenv_modules.sh
 . $my_dir/01_cleanenv_imaspy.sh
 . $my_dir/10_setenv_python.sh
-. $my_dir/20_setenv_imas_monorepo.sh
+. $my_dir/20_build_imas_git.sh master
 . $my_dir/30_setenv_imaspy.sh
 . $my_dir/35_build_imaspy.sh
 . $my_dir/36_install_imaspy.sh
+. $my_dir/80_build_docs.sh
