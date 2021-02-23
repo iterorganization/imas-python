92dd31704df3b92325329b20d5ba8e2c08ef13db
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-02-23 13:25:58 +0100

    Preprend install/bin to PATH

--- envs/iter-bamboo/10_setenv_python.sh
+++ envs/iter-bamboo/10_setenv_python.sh
@@ -6,3 +6,4 @@ export PYTHON_INSTALL_DIR=${PYTHON_INSTALL_DIR:-`make echo_env 2> /dev/null | gr
 export IMASPY_TMP_DIR=${IMASPY_TMP_DIR:-`make echo_env 2> /dev/null | grep IMASPY_TMP_DIR= | cut -d= -f2`}
 
 export PYTHONPATH=$PYTHON_INSTALL_DIR:$PYTHONPATH
+export PATH=$PYTHON_INSTALL_DIR/bin:$PATH
