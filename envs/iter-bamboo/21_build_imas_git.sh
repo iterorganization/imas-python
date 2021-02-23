dc9e3ae5a2a6ea19de5d9e81510a6cc01725a86b
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-02-23 12:18:51 +0100

    Split setting of env for IMAS build

--- envs/iter-bamboo/21_build_imas_git.sh
+++ envs/iter-bamboo/21_build_imas_git.sh
@@ -2,16 +2,6 @@
 # Set up user-like IMAS env
 set -euf -o pipefail
 
-# Use the parts of IMAS
-module load GCC/9.3.0
-module load Python/3.8.2-GCCcore-9.3.0
-module load Saxon-HE/9.9.1.7-Java-13
-module load MDSplus/7.96.12-GCCcore-9.3.0
-module load MDSplus-Java/7.96.12-GCCcore-9.3.0-Java-13
-module load HDF5/1.12.0-gompi-2020a
-module load Boost/1.72.0-GCCcore-9.3.0-no_mpi
-module list
-
 # install a DD locally
 if [ ! -d data-dictionary ]; then
     git clone ssh://git@git.iter.org/imas/data-dictionary.git
