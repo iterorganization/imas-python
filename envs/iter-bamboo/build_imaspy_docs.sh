312eafc5a8e522bd48c01bcaf5ea72d80d55797a
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-02-23 12:31:36 +0100

    Move to monorepo modules when building docs

--- envs/iter-bamboo/build_imaspy_docs.sh
+++ envs/iter-bamboo/build_imaspy_docs.sh
@@ -6,7 +6,7 @@ my_dir=$(dirname $0)
 . $my_dir/00_setenv_modules.sh
 . $my_dir/01_cleanenv_imaspy.sh
 . $my_dir/10_setenv_python.sh
-. $my_dir/20_setenv_imas_git.sh
+. $my_dir/20_setenv_imas_monorepo.sh
 . $my_dir/30_setenv_imaspy.sh
 . $my_dir/35_build_imaspy.sh
 . $my_dir/36_install_imaspy.sh
