9b1d50726feab50b38c38ae814a3459dbc21d193
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-06-08 11:04:30 +0200

    Prepare AL/DD compilation for SDCC

--- envs/iter-bamboo/20_setenv_imas_git_sdcc.sh
+++ envs/iter-bamboo/20_setenv_imas_git_sdcc.sh
@@ -1,10 +1,18 @@
 # Use the parts of IMAS
-module load GCC/9.3.0
-module load Python/3.8.2-GCCcore-9.3.0
-module load Saxon-HE/9.9.1.7-Java-13
-module load MDSplus/7.96.12-GCCcore-9.3.0
-module load MDSplus-Java/7.96.12-GCCcore-9.3.0-Java-13
-module load HDF5/1.12.0-gompi-2020a
-module load Boost/1.72.0-GCCcore-9.3.0-no_mpi
-module list
+# This is stricly versioned to catch errors early
+# Based on IMAS/3.32.1-4.9.1-2020b
+set -xe
+module load GCCcore/10.2.0
+module load Python/3.8.6-GCCcore-10.2.0
+module load MDSplus/7.96.17-GCCcore-10.2.0
+module load HDF5/1.10.7-iimpi-2020b  # todo: Intel MPI version?
+module load Boost/1.74.0-GCCcore-10.2.0
 
+# Extra modules that we need to build
+module load MDSplus-Java/7.96.17-GCCcore-10.2.0-Java-11
+module load Saxon-HE/10.3-Java-11
+# Very ugly way to just get saxon-he-10.3.jar, which we need for the DD
+export SAXONJARFILE=`echo $CLASSPATH | cut -d: -f4 | rev | cut -d/ -f1 | rev`
+echo [INFO] Set SAXONJARFILE to $SAXONJARFILE
+
+module list
