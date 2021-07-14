54da8a80a75908fd3cb7add349b4d3a1f0543ff1
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-07-14 11:02:06 +0200

    Remove spurious comment

--- envs/iter-bamboo/80_build_docs.sh
+++ envs/iter-bamboo/80_build_docs.sh
@@ -4,6 +4,5 @@ set -xe
 my_dir=$(dirname $0)
 
 # Use the sphinx matching the venv we are in using SPHINXBUILD
-# We should be in the ???? dir when running this
 VENV_SPHINX_BUILD='../venv_imaspy/bin/sphinx-build'
 make -C docs html SPHINXBUILD="$PYTHON $VENV_SPHINX_BUILD"
