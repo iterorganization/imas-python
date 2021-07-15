dae30912ef18b70316ad3535f1a6af1361daa9d5
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-07-15 10:02:52 +0200

    Split building of external modules and sdist/wheels

--- envs/iter-bamboo/35_build_imaspy.sh
+++ envs/iter-bamboo/35_build_imaspy.sh
@@ -6,4 +6,5 @@ set -xeuf -o pipefail
 # Pip install build dependencies
 $PIP install --upgrade pip setuptools
 
-$PYTHON $SETUP_PY build build_ext sdist bdist_wheel
+$PYTHON $SETUP_PY build build_ext
+$PYTHON $SETUP_PY sdist bdist_wheel
