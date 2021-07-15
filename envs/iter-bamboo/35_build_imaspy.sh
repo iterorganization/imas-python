aa89fa0ba24fe0601d4b8ac44ff5d47d292e1f54
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-07-15 11:58:20 +0200

    Split pip install command for clearer logs

--- envs/iter-bamboo/35_build_imaspy.sh
+++ envs/iter-bamboo/35_build_imaspy.sh
@@ -7,6 +7,7 @@ set -xeuf -o pipefail
 # https://packaging.python.org/guides/tool-recommendations/
 # Use setuptools to define projects.
 # Use build to create Source Distributions and wheels
-$PIP install --upgrade pip setuptools build
+$PIP install --upgrade pip
+$PIP install --upgrade setuptools build
 
 $PYTHON -m build
