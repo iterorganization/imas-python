f4b3e295b4a5d2856343a117f95cee137441fac5
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-09-17 12:51:46 +0100

    Add prototype scripts for Heimdall environment based on JINTRAC

---
+++ envs/heimdall/pytest-iter-al-git.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Set up ITER modules environment
+set -xeuf -o pipefail
+
+my_dir=$(dirname $0)
+common_dir=$(dirname $my_dir)/common
+
+. $my_dir/00_setenv_modules.sh
+. $common_dir/01_cleanenv_imaspy.sh
+. $common_dir/10_setenv_python.sh
+. $my_dir/20_setenv_imas_git.sh
+. $common_dir/22_build_python_venv.sh
+. $common_dir/25_build_imas_git.sh ${1:-develop}
+. $my_dir/../iter-bamboo/35_build_imaspy.sh
+. $common_dir/36_install_imaspy.sh
+. $common_dir/70_pytest_imaspy.sh mini
