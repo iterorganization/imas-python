ab4762a87c28d0a68134526f89eed6b73e2b8a8a
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-09-21 14:59:32 +0200

    Add common bash exit handling with shell restoration

---
+++ envs/common/00_common_bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xeuf -o pipefail
+function finish {
+  [[ "${BASH_SOURCE[0]}" != "${0}" ]] && my_name=${BASH_SOURCE[0]} || my_name=$0
+  if [[ "$my_name" == *.sh ]]; then
+    echo '[DEBUG] Clearing set and shopt flags using eval "\$one_csmd"'
+    # Wrap all set/shopt commands into a single big command
+    one_cmd=${old_bash_state//$'\n'/'; '}$'\n'
+    # Do not show the huge xtrace of this evail
+    set +x
+    eval "$one_cmd"
+    set -x
+  fi
+}
+trap finish EXIT RETURN
