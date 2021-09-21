f736cbcc4b19fa42b51a4ab5d5b36288eac8cd1d
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-09-21 17:00:32 +0200

    Use function to get current bash environment

--- envs/common/00_common_bash.sh
+++ envs/common/00_common_bash.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Boilerplate scripts and functions needed for other scripts
+function get_bash_state {
+    local bash_state="$(shopt -po; shopt -p)"; [[ -o errexit ]] && old_bash_state="$old_bash_state; set -e"  # Save bash state
+    echo $bash_state
+}
+
 function finish {
   set +x
   [[ "${BASH_SOURCE[0]}" != "${0}" ]] && my_name=${BASH_SOURCE[0]} || my_name=$0
@@ -11,4 +16,5 @@ function finish {
     eval "$one_cmd"
   fi
 }
+
 trap finish EXIT RETURN
--- envs/iter-bamboo/pytest-iter-al-git-hpc.sh
+++ envs/iter-bamboo/pytest-iter-al-git-hpc.sh
@@ -2,12 +2,12 @@
 # Build the IMASPy package and run tests on an ITER HPC-like environment
 
 # Script boilerplate
-export old_bash_state="$(shopt -po; shopt -p)"; [[ -o errexit ]] && old_bash_state="$old_bash_state; set -e"  # Save bash state
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] && my_dir=$(dirname ${BASH_SOURCE[0]}) || my_dir=$(dirname $0)  # Determine script dir even when sourcing
 
 common_dir=${my_dir}/../common
 . $common_dir/00_common_bash.sh
 
+old_bash_state=$(get_bash_state)
 set -xeuf -o pipefail # Set default script debugging flags
 
 ###############
