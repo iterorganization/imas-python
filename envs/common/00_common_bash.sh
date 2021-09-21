85a25d2b45ad605edc66a8730ccafc4e3dee3f76
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-09-21 15:54:38 +0200

    Move setting of bash flags to new location

--- envs/common/00_common_bash.sh
+++ envs/common/00_common_bash.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Boilerplate scripts and functions needed for other scripts
-set -xeuf -o pipefail
 function finish {
+  set +x
   [[ "${BASH_SOURCE[0]}" != "${0}" ]] && my_name=${BASH_SOURCE[0]} || my_name=$0
   if [[ "$my_name" == *.sh ]]; then
     echo '[DEBUG] Clearing set and shopt flags using eval "\$one_csmd"'
     # Wrap all set/shopt commands into a single big command
     one_cmd=${old_bash_state//$'\n'/'; '}$'\n'
     # Do not show the huge xtrace of this evail
-    set +x
     eval "$one_cmd"
-    set -x
   fi
 }
 trap finish EXIT RETURN
--- envs/common/10_setenv_python.sh
+++ envs/common/10_setenv_python.sh
@@ -3,11 +3,12 @@
 
 # Script boilerplate
 export old_bash_state="$(shopt -po; shopt -p)"; [[ -o errexit ]] && old_bash_state="$old_bash_state; set -e"  # Save bash state
-set -xeuf -o pipefail # Set default script debugging flags
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] && my_dir=$(dirname ${BASH_SOURCE[0]}) || my_dir=$(dirname $0)  # Determine script dir even when sourcing
 
 common_dir=$my_dir
 . $common_dir/00_common_bash.sh
+
+set -xeuf -o pipefail # Set default script debugging flags
 
 ###############
 # Script body #
