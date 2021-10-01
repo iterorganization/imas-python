78cbdb6365a50d550f0bda40638633ef8902aa6d
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2021-10-01 13:00:15 +0200

    Add safe folder copy method to MDSplus model generation
    Partial parallel files could happen. Inspired by
    https://alexwlchan.net/2019/03/atomic-cross-filesystem-moves-in-python/
    and
    https://stackoverflow.com/a/28090883

--- imaspy/mdsplus_model.py
+++ imaspy/mdsplus_model.py
@@ -12,6 +12,7 @@ import tempfile
 import uuid
 import shutil
 import getpass
+import errno
 
 from imaspy.dd_helpers import get_saxon
 from imaspy.dd_zip import get_dd_xml, get_dd_xml_crc
@@ -22,6 +23,44 @@ logger.setLevel(logging.INFO)
 
 
 MDSPLUS_MODEL_TIMEOUT = int(os.getenv("MDSPLUS_MODEL_TIMEOUT", "120"))
+
+
+def safe_move(src, dst):
+    """Rename a folder from ``src`` to ``dst``.
+
+    *   Moves must be atomic.  ``shutil.move()`` is not atomic.
+        Note that multiple threads may try to write to the cache at once,
+        so atomicity is required to ensure the serving on one thread doesn't
+        pick up a partially saved image from another thread.
+
+    *   Moves must work across filesystems.  Often temp directories and the
+        cache directories live on different filesystems.  ``os.rename()`` can
+        throw errors if run across filesystems.
+
+    So we try ``os.rename()``, but if we detect a cross-filesystem copy, we
+    switch to ``shutil.move()`` with some wrappers to make it atomic.
+    """
+    # From https://alexwlchan.net/2019/03/atomic-cross-filesystem-moves-in-python/
+    try:
+        os.rename(src, dst)
+    except OSError as err:
+
+        if err.errno == errno.EXDEV:
+            # Generate a unique ID, and copy `<src>` to the target directory
+            # with a temporary name `<dst>.<ID>.tmp`.  Because we're copying
+            # across a filesystem boundary, this initial copy may not be
+            # atomic.  We intersperse a random UUID so if different processes
+            # are copying into `<dst>`, they don't overlap in their tmp copies.
+            copy_id = uuid.uuid4()
+            tmp_dst = "%s.%s.tmp" % (dst, copy_id)
+            shutil.copytree(src, tmp_dst)
+
+            # Then do an atomic rename onto the new name, and clean up the
+            # source image.
+            os.rename(tmp_dst, dst)
+            os.rmtree(src)
+        else:
+            raise
 
 
 def mdsplus_model_dir(version, xml_file=None, rebuild=False):
@@ -110,7 +149,7 @@ def mdsplus_model_dir(version, xml_file=None, rebuild=False):
             tmp_cache_dir_path,
             cache_dir_path,
         )
-        shutil.move(tmp_cache_dir_path, cache_dir_path)
+        safe_move(tmp_cache_dir_path, cache_dir_path)
 
     return str(cache_dir_path)
 
