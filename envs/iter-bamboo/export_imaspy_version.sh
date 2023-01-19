431a99ff296ed8b9ab7333d2a5032c8c4d09ebb9
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-01-19 11:27:02 +0100

    Add script to determine IMASPy version for Bamboo

---
+++ envs/iter-bamboo/export_imaspy_version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Build the IMASPy package and run tests on an ITER SDCC-like environment
+
+# Script boilerplate
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && iter_dir=$(dirname ${BASH_SOURCE[0]}) || iter_dir=$(dirname $0)  # Determine script dir even when sourcing
+
+common_dir=${iter_dir}/../common
+. $common_dir/00_common_bash.sh
+
+old_bash_state="$(get_bash_state "$old_bash_state")"
+set -xeuf -o pipefail # Set default script debugging flags
+
+###############
+# Script body #
+###############
+
+AL_GIT_IDENTIFIER="${1:-develop}"
+# use the latest tagged version if not given
+DD_GIT_IDENTIFIER="${2:-}"
+
+. $iter_dir/00_setenv_modules.sh
+. $common_dir/01_cleanenv_imaspy.sh
+. $common_dir/10_setenv_python.sh
+. $iter_dir/20_setenv_imas_git_sdcc.sh
+source $IMASPY_VENV/bin/activate
+export IMASPY_VERSION=`$PYTHON setup.py --version`
