0e43d0786a72c39c87719f8c5351f4892188d432
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-02-08 14:55:47 +0100

    Move "tools.py" to new location

--- imaspy/command/subcommands/ids.py
+++ imaspy/command/subcommands/ids.py
@@ -1,5 +1,6 @@
+# This file is part of IMASPy.
+# You should have received IMASPy LICENSE file with this project.
 """Command-line tools provided with IMASPy."""
-
 import argparse
 import logging
 import os.path
