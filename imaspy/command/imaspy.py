eb1f1b2267141248448acad87a7fd75f8e933f29
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-02-08 14:22:23 +0100

    Add dummy "version print" click CLI function

---
+++ imaspy/command/imaspy.py
@@ -0,0 +1,20 @@
+# This file is part of IMASPy.
+# You should have received IMASPy LICENSE file with this project.
+""" Main CLI entry point """
+from importlib.metadata import version, PackageNotFoundError
+
+import click
+
+
+@click.command()
+def print_version():
+    """Print the version number of IMASPy"""
+    try:
+        __version__ = version("imaspy")
+    except PackageNotFoundError:
+        print("Package is not installed")
+    print(__version__)
+
+
+if __name__ == "__main__":
+    print_version()
