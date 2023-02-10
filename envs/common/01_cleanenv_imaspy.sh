c465a593fabd03563fd293fb3b133a63b1a968ea
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-02-10 09:41:11 +0100

    Make cleanenv script look to $XDG_CACHE_HOME

--- envs/common/01_cleanenv_imaspy.sh
+++ envs/common/01_cleanenv_imaspy.sh
@@ -15,5 +15,14 @@ set -xeuf -o pipefail # Set default script debugging flags
 ###############
 
 # Clean MDSplus model cache
-chmod u+w -R ~/.cache/imaspy || true
-rm -Rf ~/.cache/imaspy
+if [ -n "${XDG_CACHE_HOME:-}" ]; then
+    echo "XDG_CACHE_HOME set to $XDG_CACHE_HOME"
+    # IMASPy uses $XDG_CACHE_HOME/imaspy as cache folder
+    chmod u+w -R "$XDG_CACHE_HOME/imaspy" || true
+    rm -Rf "$XDG_CACHE_HOME/imaspy"
+else
+    # $XDG_CACHE_HOME unset: IMASPy uses ~/.cache/imaspy as cache folder
+    echo "XDG_CACHE_HOME unset"
+    chmod u+w -R ~/.cache/imaspy || true
+    rm -Rf ~/.cache/imaspy
+fi
