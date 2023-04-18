7410eed42ce12f033dee11db67d60fbef7c20489
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-04-18 14:17:53 +0200

    Add supposed-to-be-failing IDSMetadata deepcopy test

--- imaspy/test_ids_metadata.py
+++ imaspy/test_ids_metadata.py
@@ -1,8 +1,16 @@
+from copy import deepcopy
+
 import pytest
 
 from imaspy.dd_zip import dd_etree
 from imaspy.ids_toplevel import IDSToplevel
 from imaspy.ids_metadata import IDSMetadata
+
+
+@pytest.fixture
+def fake_structure_xml(fake_toplevel_xml):
+    tree = dd_etree(version=None, xml_path=fake_toplevel_xml)
+    return tree.find("IDS")
 
 
 def test_metadata_init():
@@ -11,8 +19,14 @@ def test_metadata_init():
     assert meta.maxoccur is None
 
 
-def test_metadata_init_structure_xml(fake_toplevel_xml):
-    tree = dd_etree(version=None, xml_path=fake_toplevel_xml)
-    ids = tree.find("IDS")
-    meta = IDSMetadata(structure_xml=ids)
-    assert ids.attrib["name"] == "gyrokinetics"
+def test_metadata_init_structure_xml(fake_structure_xml):
+    meta = IDSMetadata(structure_xml=fake_structure_xml)
+    assert fake_structure_xml.attrib["name"] == "gyrokinetics"
+
+
+def test_deepcopy(fake_structure_xml):
+    meta = IDSMetadata(structure_xml=fake_structure_xml)
+    meta2 = deepcopy(meta)
+    assert meta is not meta2
+    # assert meta.maxoccur is not meta2.maxoccur
+    assert meta == meta2
