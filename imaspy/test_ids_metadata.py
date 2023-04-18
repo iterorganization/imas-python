b4362e5370505882082435f282e2a9d2d2546434
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-04-18 14:03:49 +0200

    Add test of initializing IDSMetadata with XML structure

--- imaspy/test_ids_metadata.py
+++ imaspy/test_ids_metadata.py
@@ -1,5 +1,6 @@
 import pytest
 
+from imaspy.dd_zip import dd_etree
 from imaspy.ids_toplevel import IDSToplevel
 from imaspy.ids_metadata import IDSMetadata
 
@@ -8,3 +9,10 @@ def test_metadata_init():
     meta = IDSMetadata()
     # maxoccur should always be defined
     assert meta.maxoccur is None
+
+
+def test_metadata_init_structure_xml(fake_toplevel_xml):
+    tree = dd_etree(version=None, xml_path=fake_toplevel_xml)
+    ids = tree.find("IDS")
+    meta = IDSMetadata(structure_xml=ids)
+    assert ids.attrib["name"] == "gyrokinetics"
