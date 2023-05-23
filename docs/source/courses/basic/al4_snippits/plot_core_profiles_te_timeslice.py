d3f5c467f20e7b9d0d0376c845d665fa666e55f2
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-05-23 13:43:36 +0200

    Make plot example standalone

--- docs/source/courses/basic/al4_snippits/plot_core_profiles_te_timeslice.py
+++ docs/source/courses/basic/al4_snippits/plot_core_profiles_te_timeslice.py
@@ -1,10 +1,19 @@
 import matplotlib, os
+import imas
 
 # To avoid possible display issues when Matplotlib uses a non-GUI backend
 if 'DISPLAY' not in os.environ:
     matplotlib.use('agg')
 else:
     matplotlib.use('TKagg')
+
+shot,run,user,database = 134173,106,'public','ITER'
+input = imas.DBEntry(imas.imasdef.MDSPLUS_BACKEND,database,shot,run,user)
+input.open()
+
+# Read Te profile and the associated normalised toroidal flux coordinate
+te = input.partial_get('core_profiles','profiles_1d(261)/electrons/temperature')
+rho = input.partial_get('core_profiles','profiles_1d(261)/grid/rho_tor_norm')
 
 # Plot the figure
 import matplotlib.pyplot as plt
