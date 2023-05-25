5c796faaf96a08509e1cdfd80fc364d13319b882
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-05-25 13:08:40 +0200

    Add extendable support for future course dirs

--- imaspy/test/test_snippets.py
+++ imaspy/test/test_snippets.py
@@ -4,16 +4,17 @@ import os
 
 import pytest
 
-# al4_examples = Path(__file__, '../..', 'scripts').resolve().glob('*.py')
 courses = Path(__file__, "../../../", "docs/source/courses").resolve()
 basic_course = courses / "basic"
-basic_al4_snippets = (basic_course / "al4_snippets").glob("*.py")
+course_snippets = []
+for course in ["basic"]:
+    course_snippets.extend((courses / course).glob("*snippets/*.py"))
 
 
 @pytest.mark.skipif(
     "IMAS_HOME" not in os.environ,
     reason="IMAS_HOME must be set for tests that use the public IMAS database",
 )  # /work/imas on SDCC
-@pytest.mark.parametrize("snippets", basic_al4_snippets)
+@pytest.mark.parametrize("snippets", course_snippets)
 def test_script_execution(snippets):
     runpy.run_path(str(snippets))
