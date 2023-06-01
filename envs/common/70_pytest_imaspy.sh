0bd9a7830d54fe5076a223d2af501ec6f82d8229
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-06-01 14:34:26 +0200

    Generate HTML report of code coverage

--- envs/common/70_pytest_imaspy.sh
+++ envs/common/70_pytest_imaspy.sh
@@ -24,7 +24,7 @@ RUNMODE="${2:-normal}"
 
 if [ "$RUNMODE" == "normal" ] || [ "$RUNMODE" == "env" ]; then
     export PYTEST_FLAGS=${PYTEST_FLAGS:-'-n=auto'}
-    export COV_FLAGS=${COV_FLAGS:-'--cov=imaspy --cov-report=term --cov-report=xml:./coverage.xml'}
+    export COV_FLAGS=${COV_FLAGS:-'--cov=imaspy --cov-report=term --cov-report=xml:./coverage.xml --cov-report=html'}
     export JUNIT_FLAGS=${JUNIT_FLAGS:-'--junit-xml=./junit.xml'}
     export PYTEST_MARK=${PYTEST_MARK:-''}
     export PYTEST_FILE_OR_DIR=${PYTEST_FILE_OR_DIR:-'../imaspy'}
