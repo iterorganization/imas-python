c331a6cb9c74691c0bcb375c00a7e5bcb30be10e
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-08-04 12:58:16 +0200

    Implement tests for absolute path grabbing from mixin

--- imaspy/test/test_ids_mixin.py
+++ imaspy/test/test_ids_mixin.py
@@ -33,3 +33,9 @@ def test_toplevel(fake_filled_toplevel):
     top = fake_filled_toplevel
     assert top.wavevector._toplevel == top
     assert top.wavevector[0].radial_component_norm._toplevel == top
+
+def test_path(fake_filled_toplevel):
+    top = fake_filled_toplevel
+    assert top.wavevector._path == "/gyrokinetics/wavevector"
+    assert top.ids_properties.creation_date._path == "/gyrokinetics/ids_properties/creation_date"
+    assert top.wavevector[0].radial_component_norm._path == "/gyrokinetics/wavevector/1/radial_component_norm"
