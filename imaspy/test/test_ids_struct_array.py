7d8d5ac82dc01ae80c21a60fb82217095da7bbb2
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-08-15 13:01:04 +0200

    Change specifically generated subclasses to using common IDSToplevel

--- imaspy/test/test_ids_struct_array.py
+++ imaspy/test/test_ids_struct_array.py
@@ -3,23 +3,12 @@ import pprint
 
 import pytest
 
-from imaspy.ids_toplevel import IDSToplevel
 from imaspy.ids_struct_array import IDSStructArray
 
 
-class fake_parent_factory:
-    _path = ""
-
-
 @pytest.fixture
-def struct_array(fake_structure_xml):
-    wavevector_xml = fake_structure_xml.find(".//*[@name='wavevector']")
-    fake_structure_xml.remove(wavevector_xml)
-    top = IDSToplevel(
-        fake_parent_factory,
-        fake_structure_xml,
-    )
-    struct_array = IDSStructArray(top, wavevector_xml)
+def struct_array(fake_filled_toplevel) -> IDSStructArray:
+    struct_array = fake_filled_toplevel.wavevector
     struct_array.resize(3)
     assert len(struct_array.value) == 3
     return struct_array
--- imaspy/test/test_ids_structure.py
+++ imaspy/test/test_ids_structure.py
@@ -1,28 +1,15 @@
 # This file is part of IMASPy.
 # You should have received the IMASPy LICENSE file with this project.
-from copy import deepcopy
 import pprint
 
 import pytest
 
-from imaspy.ids_toplevel import IDSToplevel
 from imaspy.ids_structure import IDSStructure
 
 
-class fake_parent_factory:
-    _path = ""
-
-
 @pytest.fixture
-def structure(fake_structure_xml):
-    ids_properties_xml = fake_structure_xml.find(".//*[@name='ids_properties']")
-    fake_structure_xml.remove(ids_properties_xml)
-    top = IDSToplevel(
-        fake_parent_factory,
-        fake_structure_xml,
-    )
-    structure = IDSStructure(top, ids_properties_xml)
-    return structure
+def structure(fake_filled_toplevel) -> IDSStructure:
+    yield fake_filled_toplevel.ids_properties
 
 
 def test_pretty_print(structure):
