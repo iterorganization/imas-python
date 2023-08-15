af6b741c2d0318f2dd7e8adaa6d3fb3cca0f86c3
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-08-15 13:03:00 +0200

    Remove specific IDSMixin test

--- imaspy/test/test_ids_mixin.py
+++ imaspy/test/test_ids_mixin.py
@@ -10,30 +10,6 @@ from imaspy.ids_structure import IDSStructure
 from imaspy.ids_toplevel import IDSToplevel
 
 
-class fake_parent_factory:
-    pass
-
-
-class fake_array_parent:
-    _path = "/fake/parent"
-
-
-@pytest.fixture
-def mixin(fake_structure_xml):
-    wavevector_xml = fake_structure_xml.find(".//*[@name='wavevector']")
-    fake_structure_xml.remove(wavevector_xml)
-    top = IDSToplevel(
-        fake_parent_factory,
-        fake_structure_xml,
-    )
-    mixin = IDSMixin(top, wavevector_xml)
-    return mixin
-
-
-def test_time_mode(mixin):
-    assert mixin._time_mode == -999999999
-
-
 def test_toplevel(fake_filled_toplevel):
     top = fake_filled_toplevel
     assert top.wavevector._toplevel == top
