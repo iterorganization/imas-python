cad53b4d58d9292772f30f596d17fd63fe4689c2
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2023-09-04 15:34:36 +0200

    Disable napoleon generation of __init__ and private methods
    This is handled by our custom template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -334,8 +334,8 @@ autosummary_generate = True
 # See https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = True
+# napoleon_include_init_with_doc = False  # Handled by our Sphinx template
+# napoleon_include_private_with_doc = False # Handled by our Sphinx template
 # napoleon_include_special_with_doc = True
 # napoleon_use_admonition_for_examples = False
 # napoleon_use_admonition_for_notes = False
