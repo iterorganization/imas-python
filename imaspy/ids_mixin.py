89760b7b23d274e3d5fbdf616c6857d97f20a76b
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-09-06 11:27:15 +0200

    Remove `time_axis` property and move resample to `util` module
    - `time_axis` property can be requested as `coordinates.time_index` instead
    - Both `time_axis` and `resample` could shadow DD attributes, so `resample` is moved to util (similar to `visit_children`
    
    Also cleaned up imports of util.py and ids_mixin.py

--- imaspy/ids_mixin.py
+++ imaspy/ids_mixin.py
@@ -1,13 +1,9 @@
 # This file is part of IMASPy.
 # You should have received the IMASPy LICENSE file with this project.
 
-import copy
 import logging
-from typing import Dict
+from typing import TYPE_CHECKING, Dict
 from xml.etree.ElementTree import Element
-
-import scipy.interpolate
-from imaspy.ids_data_type import IDSDataType
 
 try:
     from functools import cached_property
@@ -15,16 +11,13 @@ except ImportError:
     from cached_property import cached_property
 
 from imaspy.exception import ValidationError
+from imaspy.ids_defs import IDS_TIME_MODE_INDEPENDENT
 from imaspy.ids_metadata import IDSMetadata
-from imaspy.setup_logging import root_logger as logger
-from imaspy.util import visit_children
 
-try:
-    from imaspy.ids_defs import IDS_TIME_MODE_HOMOGENEOUS, IDS_TIME_MODE_INDEPENDENT
-except ImportError as ee:
-    logger.critical("IMAS could not be imported. UAL not available! %s", ee)
+if TYPE_CHECKING:
+    from imaspy.ids_toplevel import IDSToplevel
 
-logger.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class IDSMixin:
@@ -117,69 +110,6 @@ class IDSMixin:
         my_repr += f" (IDS:{self._toplevel.metadata.name},"
         my_repr += f" {self._path}"
         return my_repr
-
-    def resample(
-        self, old_time, new_time, homogeneousTime=None, inplace=False, **kwargs
-    ):
-        """Resample all primitives in their time dimension to a new time array"""
-        if "ids_properties" in self and homogeneousTime is None:
-            homogeneousTime = self.ids_properties.homogeneous_time
-
-        if homogeneousTime is None:
-            raise ValueError(
-                "homogeneous_time is not specified in ids_properties nor given"
-                " as keyword argument"
-            )
-
-        if homogeneousTime != IDS_TIME_MODE_HOMOGENEOUS:
-            # TODO: implement also for IDS_TIME_MODE_INDEPENDENT
-            # (and what about converting between time modes? this gets tricky fast)
-            raise NotImplementedError(
-                "resample is only implemented for IDS_TIME_MODE_HOMOGENEOUS"
-            )
-
-        # we need to import here to avoid circular dependencies
-        from imaspy.ids_toplevel import IDSToplevel
-
-        def visitor(el):
-            if not el.has_value:
-                return
-            if (
-                getattr(el, "_var_type", None) == "dynamic"
-                and el.metadata.name != "time"
-            ):
-                # effectively a guard to get only idsPrimitive
-                # TODO: also support time axes as dimension of IDSStructArray
-                if el.time_axis is None:
-                    logger.warning(
-                        "No time axis found for dynamic structure %s", self._path
-                    )
-                interpolator = scipy.interpolate.interp1d(
-                    old_time.value, el.value, axis=el.time_axis, **kwargs
-                )
-                el.value = interpolator(new_time)
-
-        if not inplace:
-            el = copy.deepcopy(self)
-        else:
-            el = self
-
-        visit_children(visitor, el)
-
-        if isinstance(el, IDSToplevel):
-            el.time = new_time
-        else:
-            logger.warning(
-                "Performing resampling on non-toplevel. "
-                "Be careful to adjust your time base manually"
-            )
-
-        return el
-
-    @cached_property
-    def time_axis(self):
-        """Return the time axis for this node (None if no time dependence)"""
-        return self.coordinates.time_index
 
     @cached_property
     def _toplevel(self) -> "IDSToplevel":
--- imaspy/util.py
+++ imaspy/util.py
@@ -1,6 +1,17 @@
 # This file is part of IMASPy.
 # You should have received the IMASPy LICENSE file with this project.
 
+import copy
+import logging
+
+import scipy.interpolate
+
+from imaspy.ids_defs import IDS_TIME_MODE_HOMOGENEOUS
+from imaspy.ids_primitive import IDSPrimitive
+from imaspy.ids_toplevel import IDSToplevel
+
+logger = logging.getLogger(__name__)
+
 
 def visit_children(func, node, leaf_only=True):
     """Apply a function to node and its children
@@ -28,10 +39,6 @@ def visit_children(func, node, leaf_only=True):
                 toplevel,
             )
     """
-    from imaspy.ids_primitive import (
-        IDSPrimitive,
-    )  # pylint: disable=import-outside-toplevel
-
     if isinstance(node, IDSPrimitive):
         # Leaf node
         func(node)
@@ -40,3 +47,57 @@ def visit_children(func, node, leaf_only=True):
             func(node)
         for child in node:
             visit_children(func, child, leaf_only)
+
+
+def resample(node, old_time, new_time, homogeneousTime=None, inplace=False, **kwargs):
+    """Resample all primitives in their time dimension to a new time array"""
+    if homogeneousTime is None:
+        homogeneousTime = node._time_mode
+
+    if homogeneousTime is None:
+        raise ValueError(
+            "homogeneous_time is not specified in ids_properties nor given"
+            " as keyword argument"
+        )
+
+    if homogeneousTime != IDS_TIME_MODE_HOMOGENEOUS:
+        # TODO: implement also for IDS_TIME_MODE_INDEPENDENT
+        # (and what about converting between time modes? this gets tricky fast)
+        raise NotImplementedError(
+            "resample is only implemented for IDS_TIME_MODE_HOMOGENEOUS"
+        )
+
+    def visitor(el):
+        if not el.has_value:
+            return
+        if el.metadata.type.is_dynamic and el.metadata.name != "time":
+            # effectively a guard to get only idsPrimitive
+            # TODO: also support time axes as dimension of IDSStructArray
+            time_axis = None
+            if hasattr(el, "coordinates"):
+                time_axis = el.coordinates.time_index
+            if time_axis is None:
+                logger.warning(
+                    "No time axis found for dynamic structure %s", node._path
+                )
+            interpolator = scipy.interpolate.interp1d(
+                old_time.value, el.value, axis=time_axis, **kwargs
+            )
+            el.value = interpolator(new_time)
+
+    if not inplace:
+        el = copy.deepcopy(node)
+    else:
+        el = node
+
+    visit_children(visitor, el)
+
+    if isinstance(el, IDSToplevel):
+        el.time = new_time
+    else:
+        logger.warning(
+            "Performing resampling on non-toplevel. "
+            "Be careful to adjust your time base manually"
+        )
+
+    return el
