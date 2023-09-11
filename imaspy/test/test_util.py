8266ac99ba1dc27d549da2238fa5074f7846ce25
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-09-11 16:10:05 +0200

    Unit tests for added utility functions

---
+++ imaspy/test/test_util.py
@@ -0,0 +1,21 @@
+import imaspy.util
+
+from imaspy.test.test_helpers import fill_consistent
+
+
+def test_inspect():
+    cp = imaspy.IDSFactory("3.39.0").new("core_profiles")
+    imaspy.util.inspect(cp)  # IDSToplevel
+    imaspy.util.inspect(cp.ids_properties)  # IDSStructure
+    cp.profiles_1d.resize(5)
+    imaspy.util.inspect(cp.profiles_1d)  # IDSStructArray
+    imaspy.util.inspect(cp.profiles_1d[1])  # IDSStructure inside array
+    imaspy.util.inspect(cp.profiles_1d[1].grid)  # IDSStructure inside array
+    imaspy.util.inspect(cp.profiles_1d[1].grid.rho_tor_norm)  # IDSPrimitive
+
+
+def test_print_tree():
+    cp = imaspy.IDSFactory("3.39.0").new("core_profiles")
+    fill_consistent(cp)
+    imaspy.util.print_tree(cp)  # Full IDS tree
+    imaspy.util.print_tree(cp.ids_properties)  # Sub-tree
