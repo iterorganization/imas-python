4f05f814e040e0acf4eca65a99f0af1c3dd34361
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-09-13 16:20:57 +0200

    Add transform with imaspy code snippet

---
+++ docs/source/courses/basic/imaspy_snippets/transform_grid.py
@@ -0,0 +1,113 @@
+import os
+
+import imaspy.training
+import matplotlib
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+
+if "DISPLAY" not in os.environ:
+    matplotlib.use("agg")
+else:
+    matplotlib.use("TKagg")
+
+import matplotlib.pyplot as plt
+
+# Open input data entry
+entry = imaspy.training.get_training_db_entry()
+assert isinstance(entry, imaspy.DBEntry)
+
+# Lazy-loaded input equilibrium
+# FIXME: Add lazy=True after merge with lazy loading PR
+eq_in = entry.get("equilibrium")  # , lazy=True)
+input_times = eq_in.time.value
+
+# Create output data entry
+output_entry = imaspy.DBEntry(imaspy.ids_defs.MEMORY_BACKEND, "imaspy-course", 2, 1)
+output_entry.create()
+
+# FIXME: workaround for a bug in the MEMORY BACKEND: doing get_slice after a get leads
+# to a SEGFAULT...
+# - Check if the problem is also present when using lazy loading on line 12
+# - If not, we can remove this re-opening. Otherwise: raise a JIRA ticket for the memory
+#   backend and refer to it to explain why we're opening the entry again.
+entry = imaspy.training.get_training_db_entry()
+assert isinstance(entry, imaspy.DBEntry)
+
+
+for time in input_times:
+    eq = entry.get_slice("equilibrium", time, imaspy.ids_defs.CLOSEST_INTERP)
+
+    # Update comment
+    eq.ids_properties.comment = "IMASPy training: transform coordinate system"
+
+    p2d = eq.time_slice[0].profiles_2d[0]
+    r, z = p2d.grid.dim1.value, p2d.grid.dim2.value
+    r_axis = eq.time_slice[0].global_quantities.magnetic_axis.r.value
+    z_axis = eq.time_slice[0].global_quantities.magnetic_axis.z.value
+
+    # Create new rho/theta coordinates
+    theta = np.linspace(-np.pi, np.pi, num=64, endpoint=False)
+    max_rho = min(
+        r_axis - r[0],
+        r[-1] - r_axis,
+        z_axis - z[0],
+        z[-1] - z_axis,
+    )
+    rho = np.linspace(0, max_rho, num=64)
+
+    # Calculate corresponding R/Z for interpolating the original values
+    rho_grid, theta_grid = np.meshgrid(rho, theta, indexing="ij", sparse=True)
+    grid_r = r_axis + rho_grid * np.cos(theta_grid)
+    grid_z = z_axis + rho_grid * np.sin(theta_grid)
+    interpolation_points = np.dstack((grid_r.flatten(), grid_z.flatten()))
+
+    # Interpolate all data nodes on the new grid
+    for data_node in ["b_field_r", "b_field_z", "b_field_tor", "psi"]:
+        data = p2d[data_node].value
+        interp = RegularGridInterpolator((r, z), data)
+        new_data = interp(interpolation_points).reshape(grid_r.shape)
+        p2d[data_node].value = new_data
+
+    # Update coordinate identifier
+    p2d.grid_type.index = 2
+    p2d.grid_type.name = "inverse"
+    p2d.grid_type.description = "Rhopolar_polar 2D polar coordinates (rho=dim1, theta=dim2) with magnetic axis as centre of grid; theta and values following the COCOS=11 convention; the polar angle is theta=atan2(z-zaxis,r-raxis)"  # noqa: E501
+
+    # Update coordinates
+    p2d.grid.dim1 = rho
+    p2d.grid.dim2 = theta
+    p2d.r = grid_r
+    p2d.z = grid_z
+
+    # Finally, put the slice to disk
+    output_entry.put_slice(eq)
+
+# Create a plot to verify the transformation is correct
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, layout="tight")
+
+vmin, vmax = np.min(data), np.max(data)
+contour_levels = np.linspace(vmin, vmax, 32)
+
+rzmesh = np.meshgrid(r, z, indexing="ij")
+mesh = ax1.pcolormesh(*rzmesh, data, vmin=vmin, vmax=vmax)
+ax1.contour(*rzmesh, data, contour_levels, colors='black')
+
+ax2.pcolormesh(grid_r, grid_z, new_data, vmin=vmin, vmax=vmax)
+ax2.contour(grid_r, grid_z, new_data, contour_levels, colors='black')
+
+rho_theta_mesh = np.meshgrid(rho, theta, indexing="ij")
+ax3.pcolormesh(*rho_theta_mesh, new_data, vmin=vmin, vmax=vmax)
+ax3.contour(*rho_theta_mesh, new_data, contour_levels, colors='black')
+
+ax1.set_xlabel("r [m]")
+ax2.set_xlabel("r [m]")
+ax1.set_ylabel("z [m]")
+ax2.set_xlim(ax1.get_xlim())
+ax2.set_ylim(ax1.get_ylim())
+ax3.set_xlabel(r"$\rho$ [m]")
+ax3.set_ylabel(r"$\theta$ [rad]")
+
+fig.suptitle(r"$\psi$ in ($r,z$) and ($\rho,\theta$) coordinates.")
+fig.colorbar(mesh, ax=ax3)
+
+plt.show()
