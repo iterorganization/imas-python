ecea391bdb9b9c5555b356cb0432d0d234c00c42
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-10-11 10:35:35 +0200

    Disable unused sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -112,14 +112,14 @@ extensions = [
     # 'sphinx.ext.doctest',  # numpy
     "sphinx.ext.autosummary",  # For summarizing autodoc-generated files
     "sphinx.ext.extlinks",  # For shortening internal links
-    "sphinx.ext.graphviz",  # Draw Graphs in docs
+    # "sphinx.ext.graphviz",  # Draw Graphs in docs
     # 'sphinx.ext.ifconfig',  # numpy
     # 'matplotlib.sphinxext.plot_directive',  # numpy
     # 'IPython.sphinxext.ipythoGn_console_highlighting',  # numpy
     # 'IPython.sphinxext.ipython_directive',  # numpy
     "sphinx.ext.mathjax",  # Render math as images
     "recommonmark",  # For markdown support, does not support 'full' CommonMark syntax (yet)!
-    "sphinxcontrib.mermaid",  # Draw graphs using Mermaid.js
+    # "sphinxcontrib.mermaid",  # Draw graphs using Mermaid.js
     "sphinx_tabs.tabs", # Toggleable HTML tabs
     "sphinx_immaterial",  # Sphinx immaterial theme
 ]
