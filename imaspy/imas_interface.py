a6c9c541430576b5158e545b077b367a0b208f73
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-10-17 16:34:44 +0200

    Add logic for partial loading imas when using the IMAS loader

--- imaspy/imas_interface.py
+++ imaspy/imas_interface.py
@@ -15,11 +15,13 @@ The whole imas module is also available as a lazy-loaded module:
 <class 'imas.core_profiles.core_profiles'>
 
 """
+import fnmatch
 import importlib.machinery
 import importlib.util
 import logging
 import os.path
 import sys
+import sysconfig
 
 logger = logging.getLogger(__name__)
 
@@ -42,25 +44,25 @@ def _lazy_import(name):
     return module
 
 
-def _import_extension_submodule(package, submodule):
-    return _import_submodule(package, submodule, importlib.machinery.EXTENSION_SUFFIXES)
+def _import_extension_submodule(package_spec, submodule):
+    return _import_submodule(
+        package_spec, submodule, importlib.machinery.EXTENSION_SUFFIXES
+    )
 
 
-def _import_submodule(package, submodule, extensions):
-    fullname = f"{package}.{submodule}"
-    package_spec = importlib.util.find_spec(package)
+def _import_submodule(package_spec, submodule, extensions):
+    fullname = f"imas.{submodule}"
     spec = None
     for search_location in package_spec.submodule_search_locations:
         for ext in extensions:
             location = f"{search_location}/{submodule}{ext}"
             if os.path.exists(location):
-                spec = importlib.util.spec_from_file_location(submodule, location)
+                spec = importlib.util.spec_from_file_location(fullname, location)
                 break
         if spec:
             break
     if not spec:
         msg = "Spec for %s not found from file location, fall back to find_spec."
-        breakpoint()
         logger.debug(msg, fullname)
         spec = importlib.util.find_spec(fullname)
 
@@ -71,6 +73,38 @@ def _import_submodule(package, submodule, extensions):
 
 
 _imas_spec = importlib.util.find_spec("imas")
+if _imas_spec and _imas_spec.submodule_search_locations is None:
+    # We found the IMAS loader, do some magic to find the "real" imas package
+    pyver = ".".join(str(i) for i in sys.version_info[:2])
+
+    path = os.path.join(
+        os.getenv("IMAS_PREFIX"), "python", "lib." + sysconfig.get_platform() + "-" + pyver
+    )
+    if not os.path.isdir(path):
+        path = os.path.join(
+            os.getenv("IMAS_PREFIX"),
+            "python",
+            "lib." + sysconfig.get_platform() + "-cpython-" + pyver.replace(".", ""),
+        )
+
+    if os.path.isdir(path):
+        # Find the module file name
+        names = os.listdir(path)
+        namepattern = "imas_" + os.getenv("IMAS_VERSION").replace(".", "_") + "_ual_*"
+        namecatchall = "imas_*_ual_*"
+        # A python module matching the name pattern with env's IMAS_VERSION is preferred,
+        # but may not be found when IMAS_VERSION does not match a proper tag name (e.g.
+        # in case of custom branch installation). Then learn the derived version string
+        # from the catchall, if it finds exactly 1 in this path.
+        name = fnmatch.filter(names, namepattern)
+        if len(name) != 1:
+            name = fnmatch.filter(names, namecatchall)
+        if len(name) == 1:
+            # Found a single match, use this name
+            name = name[0]
+            sys.path.append(path)
+            _imas_spec = importlib.util.find_spec(name)
+
 if _imas_spec is None:
     has_imas = False
     imasdef = None
@@ -79,19 +113,27 @@ if _imas_spec is None:
     logger.critical(
         "Module 'imas' could not be imported. Some functionality is not available."
     )
+
+elif _imas_spec and _imas_spec.submodule_search_locations is None:
+    # Give up and just import the actual package...
+    has_imas = True
+    imas = importlib.import_module("imas")
+    lowlevel = importlib.import_module("imas._ual_lowlevel")
+    imasdef = importlib.import_module("imas.imasdef")
+
 else:
     has_imas = True
 
     # Required by _ual_lowlevel, will import all of imas if we don't import these first
-    imasdef = _import_submodule("imas", "imasdef", [".py"])
-    hli_exception = _import_submodule("imas", "hli_exception", [".py"])
+    imasdef = _import_submodule(_imas_spec, "imasdef", [".py"])
+    hli_exception = _import_submodule(_imas_spec, "hli_exception", [".py"])
 
     # Hack to make the "from hli_exception import ..." and "from imasdef import ..." of
     # _ual_lowlevel.pyx work
-    _modules = {"imasdef": imasdef, "hli_exception": hli_exception}
+    _modules = {"imasdef": imasdef, "hli_exception": hli_exception, "imas": object()}
     _old_modules = {mod: sys.modules.get(mod) for mod in _modules}
     sys.modules.update(_modules)
-    lowlevel = _import_extension_submodule("imas", "_ual_lowlevel")
+    lowlevel = _import_extension_submodule(_imas_spec, "_ual_lowlevel")
     # Clean up the global modules
     for mod, old_module in _old_modules.items():
         if old_module is not None:
