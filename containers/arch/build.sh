9cbe0077b205e76f5ccb6e2aa73bcb3fe7a1b102
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-11-02 11:45:43 +0100

    Remove outdated containers folder

--- containers/arch/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-git clone ssh://git@git.iter.org/imas/data-dictionary.git
-
-git clone ssh://git@git.iter.org/imas/access-layer.git
-
-git clone ssh://git@gitlab.com/klimex/imaspy.git
-
-docker build .
