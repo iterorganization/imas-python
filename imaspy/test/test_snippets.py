fbc58de64aa74de5f594200250cba2470f237cb9
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-11-03 13:43:49 +0100

    Change working directory to a temporary folder when testing snippets
    Prevent creation of ASCII IDS file in project tree when running pytest

--- imaspy/test/test_snippets.py
+++ imaspy/test/test_snippets.py
@@ -12,7 +12,8 @@ for course in ["basic"]:
 
 
 @pytest.mark.parametrize("snippet", course_snippets)
-def test_script_execution(snippet, monkeypatch):
+def test_script_execution(snippet, monkeypatch, tmp_path):
+    monkeypatch.chdir(tmp_path)
     # Prevent showing plots in a GUI
     monkeypatch.delenv("DISPLAY", raising=False)
     if "IMAS_HOME" not in os.environ:
