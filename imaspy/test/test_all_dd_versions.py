ad05435e8d5236f11f3812a6c894a30e1df54fe9
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-01-08 11:52:18 +0100

    Update test_all_dd_versions
    Don't use the object caches for DD etree and IDSMetadata
    Don't fill IDSs with random data - speeds up the tests, and the test was introduced to check the creation of IDSMetadata
    
    - Improves memory usage (especially when spreading the work with xdist)
    - This comes at the cost of a slight reduction in single-threaded performance

--- imaspy/test/test_all_dd_versions.py
+++ imaspy/test/test_all_dd_versions.py
@@ -1,17 +1,28 @@
 import pytest
 
-from imaspy.dd_zip import dd_xml_versions
+from imaspy import dd_zip, ids_metadata
 from imaspy.ids_factory import IDSFactory
-from imaspy.test.test_helpers import fill_with_random_data
 
 
-@pytest.fixture(params=dd_xml_versions())
+@pytest.fixture
+def skip_object_caches(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(dd_zip, "_load_etree", dd_zip._load_etree.__wrapped__)
+    monkeypatch.setattr(
+        ids_metadata,
+        "get_toplevel_metadata",
+        ids_metadata.get_toplevel_metadata.__wrapped__,
+    )
+
+
+@pytest.fixture(params=dd_zip.dd_xml_versions())
 def dd_version(request):
     return request.param
 
 
 @pytest.mark.slow
-def test_autofill_dd_version(dd_version):
+def test_create_ids_dd_version(dd_version, skip_object_caches):
+    # Test creation of all IDSs, test that IDSMetadata is correctly instantiated for
+    # all known DD verions
     factory = IDSFactory(version=dd_version)
     for ids_name in factory:
-        fill_with_random_data(factory.new(ids_name), max_children=1)
+        factory.new(ids_name)
