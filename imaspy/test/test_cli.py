7a04178509e53529d17e87e2eaf01054c766da8d
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-05-02 12:09:17 +0200

    Update unit testfor changed `imaspy version` command

--- imaspy/test/test_cli.py
+++ imaspy/test/test_cli.py
@@ -1,14 +1,11 @@
 import pytest
 from click.testing import CliRunner
 
-import imaspy
 from imaspy.command.cli import print_version
 
 
 @pytest.mark.cli
 def test_imaspy_version():
-    imaspy_version = imaspy.__version__
     runner = CliRunner()
     result = runner.invoke(print_version)
     assert result.exit_code == 0
-    assert result.output == f"{imaspy_version}\n"
