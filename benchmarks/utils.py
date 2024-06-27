fc5df8365ee28cc185013dc88966c85a0425a2ee
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-06-27 16:25:12 +0200

    Update benchmarks:
    - Drop support for AL4 and always use URIs
    - Benchmark put/get with IMASPy netCDF

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -3,52 +3,50 @@ import logging
 import uuid
 from pathlib import Path
 
-from packaging.version import Version
-
 import imaspy
-from imaspy.backends.imas_core.imas_interface import ll_interface, lowlevel
+from imaspy.backends.imas_core.imas_interface import lowlevel
 
 # Don't directly import imas: code analyzers break on the huge code base
 imas = importlib.import_module("imas")
 
 
+# Backend constants
+HDF5 = "HDF5"
+MDSPLUS = "MDSplus"
+MEMORY = "memory"
+ASCII = "ASCII"
+NETCDF = "netCDF"
+
+
+def create_uri(backend, path):
+    if backend == NETCDF:
+        return f"{path}.nc"
+    return f"imas:{backend.lower()}?path={path}"
+
+
 def backend_exists(backend):
     """Tries to detect if the lowlevel has support for the given backend."""
-    random_db = str(uuid.uuid4())
-    if ll_interface._al_version < Version("5"):
-        dbentry = imas.DBEntry(backend, random_db, 1, 1)
-        try:
-            # open() raises an exception when there is no support for the backend
-            # when there is support, but the entry cannot be opened, it only gives a
-            # negative return value.
-            # A bit weird (and subject to change per
-            # https://jira.iter.org/browse/IMAS-4671), but we can use it nicely here
-            dbentry.open()
-        except Exception as exc:
-            if "Error calling ual_begin_pulse_action" in str(exc):
-                return False
-            raise
-        return True
-
-    else:
-        dbentry = imaspy.DBEntry(backend, random_db, 1, 1)
-        try:
-            dbentry.open()
-        except Exception as exc:
-            if "backend is not available" in str(exc):
-                return False
-            elif isinstance(exc, lowlevel.ImasCoreBackendException):
-                return True
-            raise
-        return True
+    uri = create_uri(backend, str(uuid.uuid4()))
+    try:
+        entry = imaspy.DBEntry(uri, "r")
+    except Exception as exc:
+        if "backend is not available" in str(exc):
+            return False
+        elif isinstance(exc, (lowlevel.ImasCoreBackendException, FileNotFoundError)):
+            return True
+        raise
+    # Highly unlikely, but it could succeed without error
+    entry.close()
+    return True
 
 
 # Note: UDA backend is not used for benchmarking
 all_backends = [
-    imaspy.ids_defs.HDF5_BACKEND,
-    imaspy.ids_defs.MDSPLUS_BACKEND,
-    imaspy.ids_defs.MEMORY_BACKEND,
-    imaspy.ids_defs.ASCII_BACKEND,
+    HDF5,
+    MDSPLUS,
+    MEMORY,
+    ASCII,
+    NETCDF,
 ]
 
 # Suppress error logs for testing backend availabitily:
@@ -59,9 +57,7 @@ logging.getLogger().setLevel(logging.CRITICAL)
 available_backends = list(filter(backend_exists, all_backends))
 logging.getLogger().setLevel(logging.INFO)
 available_slicing_backends = [
-    backend
-    for backend in available_backends
-    if backend != imaspy.ids_defs.ASCII_BACKEND
+    backend for backend in available_backends if backend not in [ASCII, NETCDF]
 ]
 
 hlis = ["imas", "imaspy"]
@@ -77,12 +73,9 @@ available_serializers = [imaspy.ids_defs.ASCII_SERIALIZER_PROTOCOL]
 
 
 def create_dbentry(hli, backend):
+    if hli == "imas" and backend == NETCDF:
+        # Raising NotImplementedError will skip the benchmarks for this combination
+        raise NotImplementedError("AL-Python HLI doesn't implement netCDF")
+
     path = Path.cwd() / f"DB-{hli}-{backend}"
-    path.mkdir(exist_ok=True)
-    if backend == imas.imasdef.MDSPLUS_BACKEND:
-        # Need to ensure that the MDS+ data folder exists:
-        (path / "benchmark" / "3" / "0").mkdir(parents=True, exist_ok=True)
-    dbentry = DBEntry[hli](backend, "benchmark", 1, 1, str(path))
-    options = f"-prefix {path}/" if backend == imas.imasdef.ASCII_BACKEND else None
-    dbentry.create(options=options)
-    return dbentry
+    return DBEntry[hli](create_uri(backend, path), "w")
