66ba7d9df8b7fc7eae6eea79be0014f25f0f9309
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-11-05 15:17:02 +0100

    Explicitly set `data_version=3` for snippets using public SDCC DBEntries

--- docs/source/courses/basic/imaspy_snippets/explore_public_ec_launchers.py
+++ docs/source/courses/basic/imaspy_snippets/explore_public_ec_launchers.py
@@ -1,7 +1,9 @@
 import imaspy.util
 
 # Open input data entry
-entry = imaspy.DBEntry(imaspy.ids_defs.HDF5_BACKEND, "ITER_MD", 120000, 204, "public")
+entry = imaspy.DBEntry(
+    imaspy.ids_defs.HDF5_BACKEND, "ITER_MD", 120000, 204, "public", data_version="3"
+)
 entry.open()
 
 # Get the ec_launchers IDS
