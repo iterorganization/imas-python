7acabafa266196afd29855bb6c38bd3dca1fed2c
Olivier Hoenen <Olivier.Hoenen@iter.org>
2026-02-27 13:44:37 +0100

    fixup code for linting

--- imas/training.py
+++ imas/training.py
@@ -2,8 +2,6 @@
 # You should have received the IMAS-Python LICENSE file with this project.
 """Functions that are useful for the IMAS-Python training courses."""
 
-from unittest.mock import patch
-
 try:
     from importlib.resources import files
 except ImportError:  # Python 3.8 support
@@ -15,8 +13,8 @@ import imas
 def get_training_db_entry(convert=False) -> imas.DBEntry:
     """Open and return an ``imas.DBEntry`` pointing to the training data.
 
-        Args:
-            convert: if True, converts assets to default DD version 
+    Args:
+        convert: if True, converts assets to default DD version
     """
     assets_path = files(imas) / "assets/"
     entry = imas.DBEntry(f"imas:ascii?path={assets_path}", "r")
