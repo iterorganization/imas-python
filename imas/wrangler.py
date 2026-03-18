066a1aeeec4baa79f1b3cf4629a31cf97131f1c1
Severin Denk <60154343+AreWeDreaming@users.noreply.github.com>
2026-03-18 08:42:46 -0700

    Update imas/wrangler.py
    Handle version propagation more cleanly
    
    Co-authored-by: Prasad <prasad.sawantdesai@gmail.com>

--- imas/wrangler.py
+++ imas/wrangler.py
@@ -34,7 +34,7 @@ def recursively_put(location, value, ids):
 
 def wrangle(flat: Dict, version="3.41.0") -> Dict[str, IDSToplevel]:
     wrangled = {}
-    factory = IDSFactory(version)
+   factory = IDSFactory(version) if version is not None else IDSFactory()
     for key in flat:
         ids, location = key.split(".", 1)
         if ids not in wrangled:
